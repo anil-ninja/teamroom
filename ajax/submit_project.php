643fd2580bb6b4cd8a1881dc4dfc92ab4b92c021
Rajnish <rajnish_pawar90@yahoo.com>
2014-10-05 14:18:17 +0530

    type of projeect is removed
    type of projeect is removed
    Challange posted succesfully changed to Project posted succesfully in
    ajex/submit_project page

--- ajax/submit_project.php
+++ ajax/submit_project.php
@@ -7,12 +7,11 @@ if($_POST['project_title']){
 	$project_title = $_POST['project_title'] ;
 	$project_st = $_POST['project_stmt'] ;
 	$project_eta = $_POST['project_eta'] ;
-	$type = $_POST['type'] ;
 if (strlen($project_st) < 1000) {
         mysqli_query($db_handle, "INSERT INTO projects (user_id, project_title, project_stmt, project_ETA) 
-                                VALUES ('$user_id', '$project_title', '$project_st', '$project_eta');");
+                                  VALUES ('$user_id', '$project_title', '$project_st', '$project_eta');");
     if(mysqli_error($db_handle)) { echo "Failed to Post Challange!"; }
-	else { echo "Challange posted succesfully!"; }
+	else { echo "Project posted succesfully!"; }
 }
     else {
         mysqli_query($db_handle, "INSERT INTO projects_blob (project_blob_id, project_stmt) 
@@ -22,7 +21,7 @@ if (strlen($project_st) < 1000) {
         mysqli_query($db_handle, "INSERT INTO projects (user_id, project_blob_id, project_title, project_stmt, project_ETA) 
                                 VALUES ('$user_id', '$id', '$project_title', ' ', '$project_eta');");
 	if(mysqli_error($db_handle)) { echo "Failed to Post Challange!"; }
-	else { echo "Challange posted succesfully!"; }
+	else { echo "Project posted succesfully!"; }
 }
 	mysqli_close($db_handle);
 }
