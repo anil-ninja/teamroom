f601343c19e79ac9f70ea76c21dc5953302c31cc
Rajnish <rajnish_pawar90@yahoo.com>
2014-10-09 03:53:47 +0530

    dropdown on same location
    dropdown on same location
    for delettion of comment

--- functions/delete_comment.php
+++ functions/delete_comment.php
@@ -1,12 +1,14 @@
 <?php 
 function dropDown_delete($deleteid) {
                                 echo  "<div class='pull-right'>
-                                        <a class='dropdown-toggle' data-toggle='dropdown' href='#'><span class='caret'></span></a>
-                                        <ul class='dropdown-menu' aria-labelledby='themes'>
-                                            <li><a href='http://bootswatch.com/default/'>Edit Challenge</a></li>
-                                            <li><a href='#' id='deleteComment' commentID='".$deleteid."' onclick='delcomment(".$deleteid.");' class='delete color'>Delete</a></li>
-                                            <li><a href='http://bootswatch.com/cosmo/'>Report Spam</a></li>
-                                        </ul>
+                                        <div class='list-group-item'>
+                                            <a class='dropdown-toggle' data-toggle='dropdown' href='#'><span class='caret'></span></a>
+                                            <ul class='dropdown-menu' aria-labelledby='themes'>
+                                                <li><a href='http://bootswatch.com/default/'>Edit Challenge</a></li>
+                                                <li><a href='#' id='deleteComment' commentID='".$deleteid."' onclick='delcomment(".$deleteid.");' class='delete color'>Delete</a></li>
+                                                <li><a href='http://bootswatch.com/cosmo/'>Report Spam</a></li>
+                                            </ul>
+                                        </div>
                                     </div>";
 }
 ?>
