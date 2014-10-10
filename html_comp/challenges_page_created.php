7d3a9a869307d0b7eb021e3616d57cdd6b834946
spiderninjaNB <spiderninjanb@spiderninjanb-X201EV.(none)>
2014-10-10 13:16:02 +0530

    query

--- html_comp/challenges_page_created.php
+++ html_comp/challenges_page_created.php
@@ -1,79 +1,99 @@
 
 <div class="panel panel-info" >
-                            <div class="panel-heading" >
-                                <h3 class="panel-title" >Your Challenges</h3>
-                            </div>
-                        </div>
+	<div class="panel-heading" >
+		<h3 class="panel-title" >Your Challenges</h3>
+	</div>
+</div>
                         
-                            <?php 
-                               
-                                $challange_display = mysqli_query($db_handle, ("SELECT * FROM challenges WHERE user_id=$user_id and challenge_type = '1' ORDER BY challenge_creation DESC;"));
-                                while($challange_displayRow = mysqli_fetch_array($challange_display)) {
-                                    echo "<div class='panel-body'>
-                                            <div class='list-group'>";
-                                                $chall_id = $challange_displayRow['challenge_id'];
-                                                $ch_title = $challange_displayRow['challenge_title'];
-                                                $challenge_owner_status = $challange_displayRow['challenge_status'];
-                                                echo "<div class='list-group-item'>";
-                                               
-                                                    $challange_owned = mysqli_query($db_handle, ("SELECT * FROM challenge_ownership WHERE challenge_id = ".$challange_displayRow['challenge_id'].";"));
-                                                    while ($challange_ownedRow = mysqli_fetch_array($challange_owned)) {
-                                                        $challenge_owner = $challange_ownedRow['user_id'];
-                                                        $ch_eta = $challange_ownedRow['comp_ch_ETA'] ;
-                                                        $ETA = $ch_eta*60 ;
-														$day = floor($ETA/(24*60*60)) ;
-														$daysec = $ETA%(24*60*60) ;
-														$hour = floor($daysec/(60*60)) ;
-														$hoursec = $daysec%(60*60) ;
-														$minute = floor($hoursec/60) ;
-														$remainingtime = $day." Days :".$hour." Hours :".$minute." Min" ;
-                                                        $challange_own_user = mysqli_query($db_handle, ("Select * from user_info where user_id = '$challenge_owner';"));
-                                                        $challange_own_userRow = mysqli_fetch_array($challange_own_user);
-                                                        echo "<font color = '#F1AE1E'> Owned by &nbsp <span class='color strong' style= 'color :#CAF11E;'>" .ucfirst($challange_own_userRow['first_name']). '&nbsp'. ucfirst($challange_own_userRow['last_name']). " </span> &nbsp on &nbsp".$challange_ownedRow['ownership_creation']. " &nbsp with ETA in &nbsp".$remainingtime. " </font>" ;
-                                            //dropdown fro challenge functionaities added with function
-                                                        dropDown_challenge($db_handle, $chall_id);
-                                                    }
-                                                if ($challenge_owner_status == '1') {
-                                                        echo "<font color = '#F1AE1E'> Ownership is not claimed till now </font> ";
-                                        //dropdown fro challenge functionaities added with function
-                                                       dropDown_challenge($db_handle, $chall_id);
-                                                    }
-                                        echo "<tr> <br><p align='center' style='font-size: 14pt;'  ><span style= 'color :#CAF11E;'><b>".ucfirst($ch_title)."</b></span></p><br/>";
-                                        echo str_replace("<s>","&nbsp;",$challange_displayRow['stmt']). "<br> <br>";
-                                        $commenter = mysqli_query ($db_handle, ("SELECT a.stmt,a.response_ch_id, a.challenge_id, a.user_id, b.first_name, b.last_name 
-                                                                                FROM response_challenge as a 
-                                                                                JOIN user_info as b 
-                                                                                WHERE a.challenge_id = $chall_id AND a.user_id = b.user_id ORDER BY response_ch_creation ASC;"));
-                                            while($commenterRow = mysqli_fetch_array($commenter)) {
-                                                $comment_id = $commenterRow['response_ch_id'];
-                                            echo "<div id='commentscontainer'>
-                                                    <div class='comments clearfix'>
-                                                        <div class='pull-left lh-fix'>
-                                                            <img src='img/default.gif'>
-                                                        </div>
-                                                        <div class='comment-text'>
-                                                            <span class='pull-left color strong'>";
-                                                                echo "&nbsp".ucfirst($commenterRow['first_name'])."&nbsp". ucfirst($commenterRow['last_name']) .
-                                                            "</span> ";
-                                                                if ($commenterRow['user_id'] == $user_id) {
-                                                    dropDown_delete_comment_challenge($comment_id);
-                                                    }
-                                                                echo str_repeat('&nbsp;', 2) .$commenterRow['stmt'] ."
-                                                        </div>
-                                                    </div> 
-                                                </div>";
-                                        }
-                                    echo "<div class='comments clearfix'>
-                                            <div class='pull-left lh-fix'>
-                                                <img src='img/default.gif'>&nbsp
-                                            </div>
-                                            <form action='' method='POST' class='inline-form'>
-                                                <input type='hidden' value='".$chall_id."' name='challen_id' />
-                                                <input type='text' STYLE='border: 1px solid #bdc7d8; width: auto; height: 30px;' name='ch_response' placeholder='Whats on your mind about this Challenge'/>
-                                                <button type='submit' class='btn-success btn-sm glyphicon glyphicon-play' name='chl_response' value='Post'></buton>
-                                            </form>
-                                        </div>";
-                                    echo '</tr> </div> </div> </div>';
-                                    }
-                                ?>
-                            
+<?php 
+
+$challange_display = mysqli_query($db_handle, ("(SELECT DISTINCT challenge_id, challenge_status, challenge_title, user_id, challenge_ETA, stmt, challenge_creation from challenges where
+											user_id = '$user_id' and (challenge_type = '1' OR challenge_type = '2') and blob_id = '0' ORDER BY challenge_creation DESC )
+											UNION
+											(SELECT DISTINCT a.challenge_id, a.challenge_title, a.challenge_status, a.user_id, a.challenge_ETA, b.stmt, a.challenge_creation from challenges as a
+											join blobs as b WHERE a.user_id = '$user_id' and (a.challenge_type = '1' OR a.challenge_type = '2') and 
+											a.blob_id = b.blob_id ORDER BY challenge_creation DESC);"));
+while($challange_displayRow = mysqli_fetch_array($challange_display)) {
+	echo "<div class='panel-body'>
+			<div class='list-group'>
+				<div class='list-group-item'>";
+			$chall_id = $challange_displayRow['challenge_id'];
+			$ch_title = $challange_displayRow['challenge_title'];
+			$status = $challange_displayRow['challenge_status'];
+		if ($status == '2') {
+			$challange_owned = mysqli_query($db_handle, "(SELECT DISTINCT  a.user_id, a.ownership_creation, a.comp_ch_ETA, b.first_name, b.last_name from 
+														challenge_ownership as a join user_info as b where a.challenge_id = '$chall_id' and a.user_id = b.user_id
+														ORDER BY ownership_creation DESC);") ;
+			$challange_ownedRow = mysqli_fetch_array($challange_owned) ;
+			$ch_eta = $challange_ownedRow['comp_ch_ETA'] ;
+			$fname = $challange_ownedRow['first_name'] ;
+			$lname = $challange_ownedRow['last_name'] ;
+			$time = $challange_ownedRow['ownership_creation'] ;
+			$ETA = $ch_eta*60 ;
+			$day = floor($ETA/(24*60*60)) ;
+			$daysec = $ETA%(24*60*60) ;
+			$hour = floor($daysec/(60*60)) ;
+			$hoursec = $daysec%(60*60) ;
+			$minute = floor($hoursec/60) ;
+			$remainingtime = $day." Days :".$hour." Hours :".$minute." Min" ;
+			echo "<font color = '#F1AE1E'> Owned by &nbsp <span class='color strong' style= 'color :#CAF11E;'>" .ucfirst($fname). "&nbsp". ucfirst($lname). " </span> &nbsp on &nbsp".$time. " &nbsp with ETA in &nbsp".$remainingtime. " </font>" ;
+		} else {
+			echo "<font color = '#F1AE1E'> Ownership is not claimed till now </font> ";
+		}
+		echo "<div class='pull-right'>
+				<div class='list-group-item'>
+					<a class='dropdown-toggle' data-toggle='dropdown' href='#'' id='themes'><span class='caret'></span></a>
+					<ul class='dropdown-menu' aria-labelledby='dropdown'>
+                     <li><a class='btn btn-default' href='http://bootswatch.com/default/'>Edit Challenge</a></li>
+                     <li><a class='btn btn-default' id='delChallenge' cID='".$chelangeid."' onclick='delChallenge(".$chelangeid.");'>Delete Challenge</a></li>
+                     <li><a class='btn btn-default' >Change ETA</a></li>                    
+                     <li><a class='btn btn-default' >Report Spam</a></li>
+                   </ul>
+              </div>
+            </div>";
+		echo "<br><p align='center' style='font-size: 14pt;'  ><span style= 'color :#CAF11E;'><b>".ucfirst($ch_title)."</b></span></p><br/>".
+				str_replace("<s>","&nbsp;",$challange_displayRow['stmt']). "<br> <br>";
+		$commenter = mysqli_query ($db_handle, ("(SELECT DISTINCT a.stmt, a.challenge_id, a.response_ch_id, a.user_id, b.first_name, b.last_name FROM response_challenge as a
+													JOIN user_info as b WHERE a.challenge_id = $chall_id AND a.user_id = b.user_id and a.blob_id = '0' and a.status = '1' ORDER BY response_ch_creation ASC)
+												   UNION
+												   (SELECT DISTINCT a.challenge_id, a.response_ch_id, a.user_id, b.first_name, b.last_name, c.stmt FROM response_challenge as a
+													JOIN user_info as b JOIN blobs as c WHERE a.challenge_id = '$chall_id' AND a.user_id = b.user_id and a.blob_id = c.blob_id and a.status = '1' ORDER BY response_ch_creation ASC);"));
+			while($commenterRow = mysqli_fetch_array($commenter)) {
+				$comment_id = $commenterRow['response_ch_id'];
+			echo "<div id='commentscontainer'>
+					<div class='comments clearfix'>
+						<div class='pull-left lh-fix'>
+							<img src='img/default.gif'>
+						</div>
+						<div class='comment-text'>
+							<span class='pull-left color strong'>";
+								echo "&nbsp".ucfirst($commenterRow['first_name'])."&nbsp". ucfirst($commenterRow['last_name']) .
+							"</span>".$commenterRow['stmt'] ."
+							<div class='list-group-item pull-right'>
+								<a class='dropdown-toggle' data-toggle='dropdown' href='#'' id='themes'><span class='caret'></span></a>
+								<ul class='dropdown-menu' aria-labelledby='dropdown'>
+								 <li><a class='btn btn-default' href='http://bootswatch.com/default/'>Edit Challenge</a></li>
+								 <li><a class='btn btn-default' id='delChallenge' cID='".$comment_id."' onclick='delChallenge(".$comment_id.");'>Delete Challenge</a></li>                   
+								 <li><a class='btn btn-default' >Report Spam</a></li>
+							   </ul>
+						  </div>
+						</div>
+					</div> 
+				</div>";
+		}
+	echo "<div class='comments clearfix'>
+			<div class='pull-left lh-fix'>
+				<img src='img/default.gif'>&nbsp
+			</div>
+			<form action='' method='POST' class='inline-form'>
+				<input type='hidden' value='".$chall_id."' name='challen_id' />
+				<input type='text' STYLE='border: 1px solid #bdc7d8; width: auto; height: 30px;' name='ch_response' placeholder='Whats on your mind about this Challenge'/>
+				<button type='submit' class='btn-success btn-sm glyphicon glyphicon-play' name='chl_response' value='Post'></buton>
+			</form>
+		</div>";
+	
+	echo '</tr> </div> </div> </div>';
+	
+}
+?>
+
--- html_comp/challenges_page_owned.php
+++ html_comp/challenges_page_owned.php
@@ -4,86 +4,92 @@
                             </div>
                         </div>
                         
-                            <?php 
-                                $owned_challenges = mysqli_query($db_handle, ("SELECT a.challenge_id, a.stmt, a.challenge_title, a.user_id, b.ownership_creation, b.comp_ch_ETA, c.first_name, c.last_name
-                                                                        FROM challenges as a 
-                                                                            JOIN challenge_ownership as b 
-                                                                                JOIN user_info as c 
-                                                                                    where (a.challenge_id = b.challenge_id AND b.user_id = $user_id) AND a.user_id =c.user_id ORDER BY challenge_creation DESC;"));
-                                while ($owned_challengesRow = mysqli_fetch_array($owned_challenges)) {
-									$eta = $owned_challengesRow['comp_ch_ETA'];
-									$ch_title = $owned_challengesRow['challenge_title'];
-									$time = $owned_challengesRow['ownership_creation'] ;
-									$ETA = $eta*60 ;
-									$day = floor($ETA/(24*60*60)) ;
-									$daysec = $ETA%(24*60*60) ;
-									$hour = floor($daysec/(60*60)) ;
-									$hoursec = $daysec%(60*60) ;
-									$minute = floor($hoursec/60) ;
-									$remainingtime = "to accomplish in : ".$day." Days :".$hour." Hours :".$minute." Min" ;
-									$starttimestr = (string) $time ;
-									$initialtime = strtotime($starttimestr) ;
-									$totaltime = $initialtime+$ETA ;
-									$completiontime = time() ;
-								if ($completiontime > $totaltime) { 
-									$remaining_time = "Time over" ; }
-							else {	$remaintime = ($totaltime-$completiontime) ;
-									$day = floor($remaintime/(24*60*60)) ;
-									$daysec = $remaintime%(24*60*60) ;
-									$hour = floor($daysec/(60*60)) ;
-									$hoursec = $daysec%(60*60) ;
-									$minute = floor($hoursec/60) ;
-									$sec = $hoursec%60 ;
-									$remaining_time = $day." Days :".$hour." Hours :".$minute." Min :".$sec." "."Secs" ;
-								}
-                            echo "<div class='panel-body'>
-                                    <div class='list-group'>";
-                                    echo '<tr>'. "<div class='list-group-item'>";
-                            echo "<font color = '#F1AE1E'> Challenge by &nbsp <span class='color strong' style= 'color :#CAF11E;'>" .ucfirst($owned_challengesRow['first_name']). '&nbsp'. ucfirst($owned_challengesRow['last_name']). " </span> &nbsp on &nbsp".$time. " &nbsp".$remainingtime. "&nbsp&nbsp&nbsp&nbsp&nbsp Remaining Time : ".$remaining_time."</font>" ;       
-                       //dropdown fro challenge functionaities added with function
-                            dropDown_challenge($db_handle, $owned_challengesRow['challenge_id']);
-                            echo "<td> <br> 
-									<p align='center' style='font-size: 14pt;'  ><span style= 'color :#CAF11E;'><b>".ucfirst($ch_title)."</b></span></p>
-									<br/>". str_replace("<s>","&nbsp;",$owned_challengesRow['stmt']). "</td> <br> <br>";
-                            echo '</tr>';
-                            $commenter = mysqli_query ($db_handle, ("SELECT a.stmt, a.challenge_id,a.response_ch_id, a.user_id, b.first_name, b.last_name 
-                                                                    FROM response_challenge as a 
-                                                                    JOIN user_info as b 
-                                                                    WHERE a.challenge_id = ".$owned_challengesRow['challenge_id']." AND a.user_id = b.user_id ORDER BY response_ch_creation ASC;"));
-                            while($commenterRow = mysqli_fetch_array($commenter)) {
-                                $comment_owned_id = $commenterRow['response_ch_id'];
-                            echo "
-                                    <div id='commentscontainer'>
-                                            <div class='comments clearfix'>
-                                                    <div class='pull-left lh-fix'>
-                                                            <img src='img/default.gif'>
-                                                    </div>
-                                                    <div class='comment-text'>
-                                                            <span class='pull-left color strong'>";
-                                                                echo "&nbsp". ucfirst($commenterRow['first_name'])."&nbsp". ucfirst($commenterRow['last_name']) .
-                                                            "</span> ";
-                                                                if ($commenterRow['user_id'] == $user_id) {
-                                                                    dropDown_delete_comment_challenge($db_handle, $comment_owned_id);
-                                                                }
-                                                                echo str_repeat('&nbsp;', 2) .$commenterRow['stmt'] ."
-                                                    </div>
-                                            </div> 
-                                        </div>";
-                                }
-                                echo "<div class='comments clearfix'>
-                                            <div class='pull-left lh-fix'>
-                                                <img src='img/default.gif'>&nbsp
-                                            </div>
-                                            <form action='' method='POST' class='inline-form'>
-                                                <input type='hidden' value='$user_id'/></td></tr>
-                                                <input type='hidden' value='".$owned_challengesRow['challenge_id']."' name='own_challen_id' />
-                                                <input type='text' STYLE='border: 1px solid #bdc7d8; width: auto; height: 30px;' name='own_ch_response' placeholder='Whats on your mind about this Challenge'/>
-                                                <button type='submit' class='btn-success btn-sm glyphicon glyphicon-play' name='own_chl_response' ></button>
-                                            </form>
-                                    </div>
-                                ";
-                            echo '</tr> </div> </div> </div>';
+		<?php 
+			$owned_challenges = mysqli_query($db_handle, "(SELECT DISTINCT a.challenge_id, a.challenge_title, a.user_id, a.challenge_ETA, a.stmt, a.challenge_creation,
+                                            b.first_name, b.last_name, b.contact_no,b.email from challenges as a join user_info as b where
+											a.user_id = '$user_id' and blob_id = '0' and
+											a.challenge_status = '2' and a.user_id = b.user_id ORDER BY challenge_creation DESC )
+											UNION
+											(SELECT DISTINCT a.challenge_id, a.challenge_title, a.user_id, a.challenge_ETA, c.stmt, a.challenge_creation,
+											b.first_name, b.last_name, b.contact_no,b.email from challenges as a join user_info as b
+											join blobs as c WHERE a.user_id = '$user_id' and  
+											a.blob_id = c.blob_id and  a.challenge_status = '2'  and a.user_id = b.user_id ORDER BY challenge_creation DESC);");
+			while ($owned_challengesRow = mysqli_fetch_array($owned_challenges)) {
+				$eta = $owned_challengesRow['comp_ch_ETA'];
+				$ch_title = $owned_challengesRow['challenge_title'];
+				$ch_id = $owned_challengesRow['challenge_id'];
+				$time = $owned_challengesRow['ownership_creation'] ;
+				$ETA = $eta*60 ;
+				$day = floor($ETA/(24*60*60)) ;
+				$daysec = $ETA%(24*60*60) ;
+				$hour = floor($daysec/(60*60)) ;
+				$hoursec = $daysec%(60*60) ;
+				$minute = floor($hoursec/60) ;
+				$remainingtime = "to accomplish in : ".$day." Days :".$hour." Hours :".$minute." Min" ;
+				$starttimestr = (string) $time ;
+				$initialtime = strtotime($starttimestr) ;
+				$totaltime = $initialtime+$ETA ;
+				$completiontime = time() ;
+			if ($completiontime > $totaltime) { 
+				$remaining_time = "Time over" ; }
+		else {	$remaintime = ($totaltime-$completiontime) ;
+				$day = floor($remaintime/(24*60*60)) ;
+				$daysec = $remaintime%(24*60*60) ;
+				$hour = floor($daysec/(60*60)) ;
+				$hoursec = $daysec%(60*60) ;
+				$minute = floor($hoursec/60) ;
+				$sec = $hoursec%60 ;
+				$remaining_time = $day." Days :".$hour." Hours :".$minute." Min :".$sec." "."Secs" ;
+			}
+		echo "<div class='panel-body'>
+				<div class='list-group'>
+				<div class='list-group-item'>";
+		echo "<font color = '#F1AE1E'> Challenge by &nbsp <span class='color strong' style= 'color :#CAF11E;'>" .ucfirst($owned_challengesRow['first_name']). '&nbsp'. ucfirst($owned_challengesRow['last_name']). " </span> &nbsp on &nbsp".$time. " &nbsp".$remainingtime. "&nbsp&nbsp&nbsp&nbsp&nbsp Remaining Time : ".$remaining_time."</font> <br> 
+				<p align='center' style='font-size: 14pt;'  ><span style= 'color :#CAF11E;'><b>".ucfirst($ch_title)."</b></span></p>
+				<br/>". str_replace("<s>","&nbsp;",$owned_challengesRow['stmt'])."<br> <br>";
+		$commenter = mysqli_query ($db_handle, ("(SELECT DISTINCT a.stmt, a.challenge_id, a.response_ch_id, a.user_id, b.first_name, b.last_name FROM response_challenge as a
+												JOIN user_info as b WHERE a.challenge_id = '$ch_id' AND a.user_id = b.user_id and a.blob_id = '0' and a.status = '1' ORDER BY response_ch_creation ASC)
+												   UNION
+												   (SELECT DISTINCT a.challenge_id, a.response_ch_id, a.user_id, b.first_name, b.last_name, c.stmt FROM response_challenge as a
+													JOIN user_info as b JOIN blobs as c WHERE a.challenge_id = '$ch_id' AND a.user_id = b.user_id and a.blob_id = c.blob_id and a.status = '1' ORDER BY response_ch_creation ASC);"));
+		while($commenterRow = mysqli_fetch_array($commenter)) {
+			$comment_owned_id = $commenterRow['response_ch_id'];
+		echo "
+				<div id='commentscontainer'>
+						<div class='comments clearfix'>
+								<div class='pull-left lh-fix'>
+										<img src='img/default.gif'>
+								</div>
+								<div class='comment-text'>
+										<span class='pull-left color strong'>";
+											echo "&nbsp". ucfirst($commenterRow['first_name'])."&nbsp". ucfirst($commenterRow['last_name']) .
+										"</span> &nbsp&nbsp&nbsp" .$commenterRow['stmt'] ."
+										<div class='list-group-item pull-right'>
+											<a class='dropdown-toggle' data-toggle='dropdown' href='#'' id='themes'><span class='caret'></span></a>
+											<ul class='dropdown-menu' aria-labelledby='dropdown'>
+											 <li><a class='btn btn-default' href='http://bootswatch.com/default/'>Edit Challenge</a></li>
+											 <li><a class='btn btn-default' id='delChallenge' cID='".$comment_id."' onclick='delChallenge(".$comment_id.");'>Delete Challenge</a></li>                   
+											 <li><a class='btn btn-default' >Report Spam</a></li>
+										   </ul>
+									  </div>
+								</div>
+						</div> 
+					</div>";
+			}
+			echo "<div class='comments clearfix'>
+						<div class='pull-left lh-fix'>
+							<img src='img/default.gif'>&nbsp
+						</div>
+						<form action='' method='POST' class='inline-form'>
+							<input type='hidden' value='$user_id'/></td></tr>
+							<input type='hidden' value='".$owned_challengesRow['challenge_id']."' name='own_challen_id' />
+							<input type='text' STYLE='border: 1px solid #bdc7d8; width: auto; height: 30px;' name='own_ch_response' placeholder='Whats on your mind about this Challenge'/>
+							<button type='submit' class='btn-success btn-sm glyphicon glyphicon-play' name='own_chl_response' ></button>
+						</form>
+				</div>
+			";
+		echo '</tr> </div> </div> </div>';
 
-                            }
-                            ?>
+}
+?>
                         
