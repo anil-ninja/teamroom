b14f5a1eb8e236437915609fb9e36ba303cbd8ac
Rajnish <rajnish_pawar90@yahoo.com>
2014-10-12 01:09:00 +0530

    delete comment/challenges for challenges page
    delete comment/challenges for challenges page
    has been successfully done

--- ajax/ajax.php
+++ ajax/ajax.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
 include_once "../lib/db_connect.php";
+if(isset($_SESSION['user_id']))
+    $userID = $_SESSION['user_id'];
+else 
+    header ('location:../index.php');
 
-$userID = $_SESSION['user_id'];
-
-if(isset($_POST['commentID'])){
-	$commentID = $_POST['commentID'];
+if(isset($_POST['cID'])){
+	$commentID = $_POST['cID'];
 	$sql = "DELETE FROM response_challenge WHERE response_ch_id = '$commentID' and user_id = '$userID';";
 	mysqli_query($db_handle, $sql);
 
--- js/delete_comment_challenge.js
+++ js/delete_comment_challenge.js
@@ -16,10 +16,26 @@ function del_project_comment(href){
 }
 function delcomment(href) {
               if(confirm("Do u really want to delete this comment?")){
-                var dataString = 'commentID='+ href;
+                var dataString = 'cID='+ href;
                 $.ajax({
                     type: "POST",
                     url: "ajax/ajax.php",
+                    data: dataString,
+                    cache: false,
+                    success: function(result){
+                        alert(result);
+                        location.reload();
+                    }
+                });
+              }
+            }
+            function delChallenge(href) {
+              if(confirm("Do u really want to delete this challenge?")){
+                 
+                var dataString = 'cID='+ href;
+                $.ajax({
+                    type: "POST",
+                    url: "ajax/delete_chalange.php",
                     data: dataString,
                     cache: false,
                     success: function(result){
