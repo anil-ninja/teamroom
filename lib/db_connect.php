457037c9b935c4a9941c08ee9bbab3d89a7d9a0a
rahul lahoria <rahullahoria@cap-lap-360.(none)>
2014-10-12 16:26:32 +0530

    bug fixed

--- lib/db_connect.php
+++ lib/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$db_handle = mysqli_connect("localhost","root","redhat11111p","ninjasTeamRoom");
+$db_handle = mysqli_connect("localhost","root","redhat111111","ninjasTeamRoom");
 if (mysqli_connect_errno()) {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
