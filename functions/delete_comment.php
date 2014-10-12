07ef2365d9a14aa569902d5372d6b8d3a1ea4dea
Rajnish <rajnish_pawar90@yahoo.com>
2014-10-12 22:28:41 +0530

    commnet system improved
    commnet system improved

--- functions/delete_comment.php
+++ functions/delete_comment.php
@@ -1,15 +1,22 @@
 <?php 
 //include_once './lib/db_connect.php';
 
-function dropDown_delete_comment_challenge($deleteid) {
-    echo  "<div class='pull-right'>
-            <div class='list-group-item'>
-                <a class='dropdown-toggle' data-toggle='dropdown' href='#'><span class='caret'></span></a>
-                <ul class='dropdown-menu' aria-labelledby='themes'>
-                    <li><a href='http://bootswatch.com/default/'>Edit Challenge</a></li>
-                    <li><a href='#' id='deleteComment' commentID='".$deleteid."' onclick='delcomment(".$deleteid.");' class='delete color'>Delete</a></li>
-                </ul>
-            </div>
+function dropDown_delete_comment_challenge($db_handle, $deleteid, $user_ID) {
+    echo  "<div class='list-group-item pull-right'>
+            <a class='dropdown-toggle' data-toggle='dropdown' href='#' id='themes'><span class='caret'></span></a>
+            <ul class='dropdown-menu' aria-labelledby='dropdown'>";
+            
+            $challenge_dropdown_comment = mysqli_query($db_handle, ("SELECT user_id FROM response_challenge WHERE response_ch_id = '$deleteid' AND user_id='$user_ID';"));
+                    $challenge_dropdown_commentRow = mysqli_fetch_array($challenge_dropdown_comment);
+                    $challenge_dropdown_comment_userID = $challenge_dropdown_commentRow['user_id'];
+                    if($challenge_dropdown_comment_userID == $user_ID) {
+                        echo "
+                        <li><button class='btn-link' href='#'>Edit</button></li>
+                        <li><button class='btn-link' cID='".$deleteid."' onclick='delcomment(".$deleteid.");'>Delete</button></li>";
+                    } else {
+                       echo "<li><button class='btn-link' >Report Spam</button></li>";
+                    }
+                echo "</ul>
         </div>";
 }
 function dropDown_delete_comment_project($deleteid) {
@@ -23,6 +30,7 @@ function dropDown_delete_comment_project($deleteid) {
             </div>
         </div>";
 }
+
 function dropDown_challenge($db_handle, $challenge_ID, $user_ID, $remaining_time_ETA_over) {
         echo "<div class='pull-right'>
             <div class='list-group-item'>
