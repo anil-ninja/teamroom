7d3451b19e806af28a1dc12ec939318698cd15d4
spiderninjaNB <spiderninjanb@spiderninjanb-X201EV.(none)>
2014-10-12 16:03:40 +0530

    home page ui

--- ninjas.inc.php
+++ ninjas.inc.php
@@ -73,7 +73,7 @@ if(isset($_POST['eta'])) {
 				      <select class='btn btn-default btn-xs' name = 'y_etac' ><option value='0' selected >hours</option><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option><option value='6'>6</option><option value='7'>7</option><option value='8'>8</option><option value='9'>9</option><option value='10'>10</option><option value='11'>11</option><option value='12'>12</option><option value='13'>13</option><option value='14'>14</option><option value='15'>15</option><option value='16'>16</option><option value='17'>17</option><option value='18'>18</option><option value='19'>19</option><option value='20'>20</option><option value='21'>21</option><option value='22'>22</option><option value='23'>23</option></select>&nbsp;
 				      <select class='btn btn-default btn-xs' name = 'y_etad' ><option value='15' selected >minute</option><option value='30' >30</option><option value='45' >45</option></select>
 				    <input type='hidden' name='cid' value='".$id."'><br/><br/>
-				    <input type='submit' class='btn btn-success btn-sm' name='chlange' value = 'Change' ></small>
+				    <input type='submit' class='btn btn-success btn-sm' name='change' value = 'Change' ></small>
 				  </form>
 				</div> 
 				<div class='modal-footer'>
