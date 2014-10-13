359977dbccd0c9bb752cbb91c3ef5b95a741a908
spiderninjaNB <spiderninjanb@spiderninjanb-X201EV.(none)>
2014-10-13 19:19:09 +0530

    navbar

--- html_comp/navbar_homepage.php
+++ html_comp/navbar_homepage.php
@@ -19,57 +19,125 @@
             </button>
         </form></li>
     </ul>
-    <ul class="nav navbar-nav navbar-right">
+    <ul class='nav navbar-nav navbar-right'>
+    <?php
+    if($requestedPage == "ninjas.php"){
+    echo "<li>
+		  <div class='dropdown'>
+            <a data-toggle='dropdown'><p class='navbar-text'>Your Teams<span class='caret'></span></p></a>
+    		<ul class='dropdown-menu multi-level' role='menu' aria-labelledby='dropdownMenu'>" ;
+    		 
+		$teams_name_display = mysqli_query($db_handle, ("select team_name from teams where user_id= '$user_id' ;")) ;
+		while ($teams_name_displayRow = mysqli_fetch_array($teams_name_display)) {
+				$team_name = $teams_name_displayRow['team_name'] ;
+				echo " <li class='dropdown-submenu'>
+						<a style='white-space: normal;'>".ucfirst($team_name)."<br/></a>
+								<ul class='dropdown-menu'>" ;
+																	
+			$teams_names_display = mysqli_query($db_handle, ("select b.first_name, b.last_name,a.team_name,b.email,b.contact_no,b.rank from teams as a join user_info
+																as b where a.team_name = '$team_name' AND a.user_id = b.user_id and a.member_status = '1';"));
+				while ($teams_names_displayRow = mysqli_fetch_array($teams_names_display)) {
+								$firstname = $teams_names_displayRow['first_name'] ;
+								$lastname = $teams_names_displayRow['last_name'] ;
+						echo "<li><p align='center' ><form method='GET' value='".$username."' action='profile.php'><input type='submit' class='btn-link' value='".ucfirst($firstname)." ".ucfirst($lastname)."' 
+							  style='white-space: normal;'/></form></p></li>" ;
+				}
+				echo "</ul></li>" ;
+			}
+			echo "</ul></div></li>";
+			}
+			?>
+			
 		<li>
-		  <div class="dropdown">
-            <a data-toggle="dropdown"><p class="navbar-text">Your Teams<span class="caret"></span></p></a>
-    		<ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
-              <?php 
-                            $teams_name_display = mysqli_query($db_handle, ("select team_name from teams where user_id= '$user_id' ;")) ;
-                            while ($teams_name_displayRow = mysqli_fetch_array($teams_name_display)) {
-                                    $team_name = $teams_name_displayRow['team_name'] ;
-                                    echo " <li class='dropdown-submenu'>
-											<a style='white-space: normal;'>".ucfirst($team_name)."<br/></a>
-													<ul class='dropdown-menu'>" ;
-                                                                                        
-								$teams_names_display = mysqli_query($db_handle, ("select b.first_name, b.last_name,a.team_name,b.email,b.contact_no,b.rank from teams as a join user_info
-                                                                                    as b where a.team_name = '$team_name' AND a.user_id = b.user_id and a.member_status = '1';"));
-                                    while ($teams_names_displayRow = mysqli_fetch_array($teams_names_display)) {
-                                                    $firstname = $teams_names_displayRow['first_name'] ;
-                                                    $lastname = $teams_names_displayRow['last_name'] ;
-                                                    $email = $teams_names_displayRow['email'] ;
-                                                    $phone = $teams_names_displayRow['contact_no'] ;
-                                                    $rank = $teams_names_displayRow['rank'] ;
-                                                    $profile = $email." "."Phone No. : ".$phone." "."Rank : ".$rank ;
-                                            echo "<li><p align='center' ><input type='submit' class='btn btn-default' name='projectphp' data-toggle='tooltip' 
-                                                  data-placement='bottom' data-original-title='".$profile."' value='".ucfirst($firstname)." ".ucfirst($lastname)."' 
-                                                  style='white-space: normal;'/></p></li>" ;
-                                    }
-                                    echo "</ul></li><br/>" ;
+		  <div class='dropdown'>
+            <a data-toggle='dropdown'><p class='navbar-text'>Your Projects<span class='caret'></span></p></a>
+    		<ul class='dropdown-menu multi-level' role='menu' aria-labelledby='dropdownMenu'>
+			   <?php
+                            $project_title_display = mysqli_query($db_handle, ("(SELECT DISTINCT a.project_id, b.project_title,b.project_ETA,b.project_creation FROM teams as a join projects 
+                                                                                as b WHERE a.user_id = '$user_id' and a.project_id = b.project_id and b.project_type = '1')  
+                                                                                UNION (SELECT DISTINCT project_id, project_title, project_ETA, project_creation FROM projects WHERE user_id = '$user_id' and project_type= '1');"));
+                                while ($project_title_displayRow = mysqli_fetch_array($project_title_display)) {
+                                $p_title = $project_title_displayRow['project_title'] ;		
+                                echo "<li><form method='POST' action=''>
+                                <input type='hidden' name='project_title' value='".$p_title."'/>
+                                <input type='hidden' name='project_id' value='".$project_title_displayRow['project_id']."'/>
+                                <button type='submit' class='btn-link' name='projectphp' style='white-space: pre-line;'>".$p_title."</button><br/><br/></form></li>" ;
                             }
-                          ?>
-            </ul>
-        </div>
-      </li>
-      <li><form method="POST" >
-          <?php 
-            if($requestedPage == "ninjas.php") 
-              echo "<p class='navbar-text'>
-                      <a data-toggle='modal'  data-target='#myModal' style='cursor:pointer;'>
-                            
-                            <i class='glyphicon glyphicon-edit'>
-                            </i>Create New Project
-						</a>
-                    </p>";
-          ?>
+                        ?>
+                        </ul>
+                      </div>
+                  </li>      
+      <li><form method="POST" > 
+          <p class="navbar-text"><a data-toggle="modal" class="btn-link" data-target="#createProject"><i class="glyphicon glyphicon-edit"></i>Create Project</a></p>
           <p class="navbar-text"><a href="challenges.php" style='cursor:pointer;'>Your Challenges</a></p>
           <p class="navbar-text">&nbsp;Your rank :  <?php echo $rank ; ?></p>
           <p class="navbar-text"><span class="glyphicon glyphicon-user"></span><a href="profile.php"> Hello <?php echo ucfirst($name); ?></a></p>                              
           <p class="navbar-text"><button type="submit" class="btn btn-danger btn-sm" name="logout" ><span class="glyphicon glyphicon-off"></span></button></p></form>
       </li>
-                   <li> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </li>
     </ul>
   </div>
 </div>
+<!-- Modal -->
+<div class="modal fade" id="createProject" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title" id="myModalLabel">Create New Project</h4>
+            </div>
+            <div class="modal-body">
+                <form >
+                    <div class="input-group-addon" >
+                        <input type="text" class="form-control" id="project_title" placeholder="Enter Project Title">
+                    </div>
+                    <br>
+                    <div class="input-group-addon">
+                        <textarea rows="3" class="form-control" id="project_stmt" placeholder="Details about Project"></textarea>
+                    </div>
+                    <br/>
+                    Estimated Time (ETA)
+                    <select id = "eta" >	
+                        <option value='0' selected >Month</option>
+                        <?php
+                        $m = 1;
+                        while ($m <= 11) {
+                            echo "<option value='" . $m . "' >" . $m . "</option>";
+                            $m++;
+                        }
+                        ?>
+                    </select>&nbsp;<select id = "etab" >	
+                        <option value='0' selected >Days</option>
+                        <?php
+                        $d = 1;
+                        while ($d <= 30) {
+                            echo "<option value='" . $d . "' >" . $d . "</option>";
+                            $d++;
+                        }
+                        ?>
+                    </select>&nbsp;<select id = "etac" >	
+                        <option value='0' selected >hours</option>
+                        <?php
+                        $h = 1;
+                        while ($h <= 23) {
+                            echo "<option value='" . $h . "' >" . $h . "</option>";
+                            $h++;
+                        }
+                        ?>
+                    </select>&nbsp;<select id= "etad" >	
+                        <option value='15' selected >minute</option>
+                        <option value='30' >30</option>
+                        <option value='45'  >45</option>
+                    </select>
+                    <br/><br/>
+                    <input type="button" class="btn btn-primary" id = "create_project" value = "Create Project" >
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button id="newuser" type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!--end modle-->
 
 
--- html_comp/right_panel_project.php
+++ html_comp/right_panel_project.php
@@ -15,41 +15,11 @@ include_once 'project.inc.php';
                 <br>
                 <a data-toggle="modal" class="btn btn-link" data-target="#createnotes" style="cursor:pointer;"><i class="glyphicon glyphicon-edit"></i>Enter Notes</a><br/><br/>
                 <a data-toggle="modal" class="btn btn-link" data-target="#createChallenge" style="cursor:pointer;"><i class="glyphicon glyphicon-edit"></i>Create Challenge</a><br/><br/>
-                <a data-toggle="modal" class="btn btn-link" data-target="#createProject" style="cursor:pointer;"><i class="glyphicon glyphicon-edit"></i>Create Project</a><br/><br/>
-                <a data-toggle="modal" class="btn btn-link" data-target="#create_team_new" style="cursor:pointer;"><i class="glyphicon glyphicon-edit"></i>Create Team</a><br/><br/>
-                <a href="challenges.php" class="btn btn-link" >Your Challenges</a><br/></p>	
+                <a data-toggle="modal" class="btn btn-link" data-target="#create_team_new" style="cursor:pointer;"><i class="glyphicon glyphicon-edit"></i>Create Team</a><br/>
                 </font> 
                 </p>
             </div>  
             <div class="modal-body">
-                <div class="well">
-                    <ul class="nav">
-                        <label class='tree-toggle nav-header btn btn-link' ><p align="center">Your Projects</p><br/></label>
-                        <ul class='nav tree' style='display: none;'>
-                            <?php
-                            $project_title_display = mysqli_query($db_handle, ("(SELECT DISTINCT a.project_id, b.project_title,b.project_ETA,b.project_creation FROM teams as a join projects 
-                                                                                as b WHERE a.user_id = '$user_id' and a.project_id = b.project_id)  
-                                                                                UNION (SELECT DISTINCT project_id, project_title, project_ETA, project_creation FROM projects WHERE user_id = '$user_id');"));
-                            while ($project_title_displayRow = mysqli_fetch_array($project_title_display)) {
-                                $p_title = $project_title_displayRow['project_title'] ;
-                                $p_eta = $project_title_displayRow['project_ETA'] ;
-                                $p_time = $project_title_displayRow['project_creation'] ;
-                                $eta = $p_eta*60 ;
-								$day = floor($eta/(24*60*60)) ;
-								$daysec = $eta%(24*60*60) ;
-								$hour = floor($daysec/(60*60)) ;
-								$hoursec = $daysec%(60*60) ;
-								$minute = floor($hoursec/60) ;
-								$remaining_time = "ETA : ".$day." Days :".$hour." Hours :".$minute." Min" ;
-								$title = "Project Created ON : ".$p_time." ".$remaining_time ;			
-                                echo "<form method='POST' action=''>
-                                <input type='hidden' name='project_title' value='".$p_title."'/>
-                                <input type='hidden' name='project_id' value='".$project_title_displayRow['project_id']."'/>
-                                <p align='center' ><input type='submit' class='btn btn-default' name='projectphp' data-toggle='tooltip' 
-                                data-placement='bottom' data-original-title='".$title."' value='".$p_title."' style='white-space: normal;'/></p></form><br/>" ;
-                            }
-                            ?>
-                        </ul></div>
                         <div class="well">
                     <ul class="nav">
                         <label class='tree-toggle nav-header btn-default btn-xs' ><p align="center">Your Teams</p><br/></label>
@@ -188,7 +158,7 @@ include_once 'project.inc.php';
             </div>
         </div>
     </div>
-</div></div></div>
+</div>
 <!--end modle-->
 <!-- Modal -->
 <div class="modal fade" id="createChallenge" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
@@ -270,71 +240,9 @@ include_once 'project.inc.php';
             </div>
         </div>
     </div>
-</div></div></div>
+</div>
 <!--end modle-->           
 
-<!-- Modal -->
-<div class="modal fade" id="createProject" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                <h4 class="modal-title" id="myModalLabel">Create New Project</h4>
-            </div>
-            <div class="modal-body">
-                <form >
-                    <div class="input-group-addon" >
-                        <input type="text" class="form-control" id="project_title" placeholder="Enter Project Title">
-                    </div>
-                    <br>
-                    <div class="input-group-addon">
-                        <textarea rows="3" class="form-control" id="project_stmt" placeholder="Details about Project"></textarea>
-                    </div>
-                    <br/>
-                    Estimated Time (ETA)
-                    <select id = "eta" >	
-                        <option value='0' selected >Month</option>
-                        <?php
-                        $m = 1;
-                        while ($m <= 11) {
-                            echo "<option value='" . $m . "' >" . $m . "</option>";
-                            $m++;
-                        }
-                        ?>
-                    </select>&nbsp;<select id = "etab" >	
-                        <option value='0' selected >Days</option>
-                        <?php
-                        $d = 1;
-                        while ($d <= 30) {
-                            echo "<option value='" . $d . "' >" . $d . "</option>";
-                            $d++;
-                        }
-                        ?>
-                    </select>&nbsp;<select id = "etac" >	
-                        <option value='0' selected >hours</option>
-                        <?php
-                        $h = 1;
-                        while ($h <= 23) {
-                            echo "<option value='" . $h . "' >" . $h . "</option>";
-                            $h++;
-                        }
-                        ?>
-                    </select>&nbsp;<select id= "etad" >	
-                        <option value='15' selected >minute</option>
-                        <option value='30' >30</option>
-                        <option value='45'  >45</option>
-                    </select>
-                    <br/><br/>
-                    <input type="button" class="btn btn-primary" id = "create_project" value = "Create Project" >
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button id="newuser" type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-    </div>
-</div>
-<!--end modle-->
 <div class="modal fade" id="create_team_new" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
