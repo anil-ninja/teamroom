109ea87e4b6282ef9bb3e7c29e8c340a7235ce9f
Rajnish <rajnish_pawar90@yahoo.com>
2014-10-13 12:46:01 +0530

    setting session user id with isset

--- ajax/submit_project_challenge_response.php
+++ ajax/submit_project_challenge_response.php
@@ -7,7 +7,7 @@ if($_POST['notes']){
 	$ch_id = $_POST['id'] ;
        mysqli_query($db_handle,"INSERT INTO response_challenge (user_id, challenge_id, stmt) VALUES ('$user_id', '$ch_id', '$notes') ; ") ;
     if(mysqli_error($db_handle)) { echo "Failed to Post Comment!"; }
-	else { echo "Comment posted succesfully!"; }
+	//else { echo "Comment posted succesfully!"; }
 	mysqli_close($db_handle);
 } 
 else echo "Invalid parameters!";
