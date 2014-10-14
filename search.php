5db3958dc06b3ee9d2a270111bcd88aa31f757d3
rahul lahoria <rahullahoria@cap-lap-360.(none)>
2014-10-15 03:59:51 +0530

    seach

---
+++ search.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'lib/db_connect.php';
+
+$keyword = $_GET['keyword'] ;
+$searchResults = mysqli_query($db_handle, ("SELECT challenge_id, challenge_title, LEFT(stmt, 30) as stmt 
+	FROM challenges 
+	WHERE challenge_type = '1' 
+	AND ((challenge_title like '%$keyword%' And stmt NOT LIKE '<iframe%') 
+	OR (stmt like '%$keyword%' And stmt NOT LIKE '<iframe%') 
+	);"));
+
+$data = array();
+while($row = $searchResults->fetch_assoc()){
+    $data[] = $row;
+}
+echo json_encode($data);
+
+
+?>
