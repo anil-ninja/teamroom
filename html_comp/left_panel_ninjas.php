62821168b7e2ce67d8e3edad92dd40919908fe86
neerajdexter <esmanaj@JW678R1.egi.ericsson.com>
2014-10-25 02:03:20 +0530

    all icon changed

--- html_comp/left_panel_ninjas.php
+++ html_comp/left_panel_ninjas.php
@@ -1,14 +1,15 @@
 <div class="bs-component">
               
-                      <p align="center"><font size="4">Private Projects</font></p><hr/>
+                      <p><a data-toggle='modal' class='btn btn-link' data-target='#createProject' style='cursor:pointer;'><i class='glyphicon glyphicon-plus'></a></i>
+                      <font size="2"><b>Private Projects</b></font></p><hr/>
                        <?php
                             $project_title_display = mysqli_query($db_handle, "(SELECT DISTINCT a.project_id, b.project_title,b.project_ETA,b.project_creation FROM teams as a join projects 
                                                                                 as b WHERE a.user_id = '$user_id' and a.project_id = b.project_id and b.project_type = '2')  
                                                                                 UNION (SELECT DISTINCT project_id, project_title, project_ETA, project_creation FROM projects WHERE user_id = '$user_id' and project_type= '2');");
                                 while ($project_title_displayRow = mysqli_fetch_array($project_title_display)) {
                                 $p_title = $project_title_displayRow['project_title'] ;
-                                if (strlen($p_title) > 25) {
-								$prtitle = substr(ucfirst($p_title),0,26)."....";
+                                if (strlen($p_title) > 40) {
+								$prtitle = substr(ucfirst($p_title),0,40)."...";
 								} else {
 									$prtitle = ucfirst($p_title) ;
 								}								   
@@ -52,14 +53,14 @@
                                         echo "<form method='POST' action=''>
                                         <input type='hidden' name='project_id' value='".$project_title_displayRow['project_id']."'/>
                                         <button type='submit' class='btn-link' name='projectphp' data-toggle='tooltip' 
-                                        data-placement='bottom' data-original-title='".$title."' style='height: 20px;font-size:14px;'><b>
-                                        ".$prtitle."</b></button>
+                                        data-placement='bottom' data-original-title=' ".$title."' style='height: 20px;font-size:11px;'>
+                                        <b>".$prtitle."</b></button>
                                         <br/><p style='font-size:8pt; color:rgba(161, 148, 148, 1);'>&nbsp;&nbsp;&nbsp;".$remaining_time_own."</p></form>" ;
                                     
                                 
                             }
                         ?><hr/>
-                      <p align="center"><font size="4">Public Projects</font></p><hr/>
+                      <p align="left"><font size="2">Public Projects</font></p><hr/>
                        <?php /*
                             $project_public_title_display = mysqli_query($db_handle, "SELECT DISTINCT project_id, project_title, project_ETA, project_creation FROM projects WHERE project_type= '1';");
                                 while ($project_public_title_displayRow = mysqli_fetch_array($project_public_title_display)) {
