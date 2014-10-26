d9c01782bc6f6c5eed2c093a79d6b1789d7c90f6
anil-ninja <kumar.anil8892@gmail.com>
2014-10-27 05:06:40 +0530

    upload pic

---
+++ ajax/submit_photo.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+include_once "../lib/db_connect.php";
+//print_r($_POST);
+//echo "hi";
+if($_POST['picturech']){
+	$user_id = $_SESSION['user_id'];
+	$articletext = $_POST['picturech'] ;
+	$article_title = $_POST['picture_title'] ;
+	$image = $_POST['img'] ;
+	$article = $image." ".$articletext ;
+ if (strlen($article) < 400) {
+        mysqli_query($db_handle,"INSERT INTO challenges (user_id, challenge_title, stmt, challenge_open_time, challenge_ETA, challenge_type) 
+                                    VALUES ('$user_id', '$article_title', '$article', '1', '1', '8') ; ") ;
+    if(mysqli_error($db_handle)) { echo "Failed to Post Article!"; }
+	else { echo "Posted succesfully!"; }
+} 
+else {
+        mysqli_query($db_handle, "INSERT INTO blobs (blob_id, stmt) 
+                                VALUES (default, '$article');");
+        
+        $id = mysqli_insert_id($db_handle);
+        mysqli_query($db_handle, "INSERT INTO challenges (user_id, challenge_title, blob_id, challenge_open_time, challenge_ETA, stmt, challenge_type) 
+                                VALUES ('$user_id', '$article_title', '$id', '1', '1', ' ', '8');");
+	 if(mysqli_error($db_handle)) { echo "Failed to Post Article!"; }
+	else { echo "Posted succesfully!"; }
+}
+	mysqli_close($db_handle);
+} 
+else echo "Invalid parameters!";
+?>
