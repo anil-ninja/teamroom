5be5f902f4c00fcb30241e77c3e56e5806bd1454
Rajnish <rajnish_pawar90@yahoo.com>
2014-10-27 19:56:24 +0530

    commenrt added fr test

--- lib/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$db_handle = mysqli_connect("localhost","root","redhat111111","ninjasTeamRoom");
-if (mysqli_connect_errno()) {
-	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-		
-?>
