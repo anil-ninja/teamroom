53b0b81524c54fcde984d81fe3b56952031ede07
anil-ninja <kumar.anil8892@gmail.com>
2014-10-28 02:32:59 +0530

    explode is not every time bad, creafully while playing

--- models/challenge.php
+++ models/challenge.php
@@ -23,9 +23,16 @@ class challenge{
         }
         $chalange = mysqli_query($db_handle, "SELECT * FROM challenges as a JOIN user_info as b 
                                                 Where a.challenge_id='$this->id' AND a.user_id = b.user_id;");
+        
         $chalangerow = mysqli_fetch_array($chalange);
+        
         $this->challenge_title = $chalangerow['challenge_title'];
-        $this->stmt = $chalangerow['stmt'];
+        if($chalangerow['blob_id']== 0)
+			$this->stmt = $chalangerow['stmt'];
+		else {
+			$blob_id = $chalangerow['blob_id'];
+			$this->stmt = mysqli_fetch_array(mysqli_query($db_handle, "SELECT stmt FROM blobs Where blob_id='$blob_id';"))['stmt'];
+        }
         $this->first_name = $chalangerow['first_name'];
         $this->last_name = $chalangerow['last_name'];
         $this->username = $chalangerow['username'];
@@ -34,8 +41,9 @@ class challenge{
     }
     function getDiscription(){
         if (substr($this->stmt, 0, 4) == "<img") {
-            $arrayStmt = explode("\"", $this->stmt);
-            return substr($arrayStmt[1],0,155);
+            $arrayStmt = explode(">", $this->stmt);
+            
+            return substr($this->stmt,strlen($arrayStmt[0])+1,155);
         }
         return substr($this->stmt, 0, 200);
     }
