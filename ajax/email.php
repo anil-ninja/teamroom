dd8fd4b8944e164504a62f10d5f73218cbde5c14
anil-ninja <kumar.anil8892@gmail.com>
2014-10-31 00:52:44 +0530

    assign task

--- ajax/email.php
+++ ajax/email.php
@@ -1,6 +1,7 @@
 <?php
 include_once "../lib/db_connect.php";
 if ($_POST['email']) {
+	$email = $_POST['email'] ;
 $sql="SELECT * FROM user_info where email='$email'";
 $data=mysqli_query($db_handle,$sql);
 	if(mysqli_num_rows($data)>0) {
