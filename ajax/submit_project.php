b9d80333b6400ba9de65b01016c316b1ca237d03
anil-ninja <kumar.anil8892@gmail.com>
2014-10-31 21:36:05 +0530

    ninja page

--- ajax/submit_project.php
+++ ajax/submit_project.php
@@ -5,12 +5,16 @@ include_once '../ninjas.inc.php';
 if($_POST['project_title']){
 	$user_id = $_SESSION['user_id'];
 	$project_title = $_POST['project_title'] ;
-	$project_st = htmlspecialchars(trim($_POST['project_stmt']), ENT_QUOTES);
+	$project_sttext = htmlspecialchars(trim($_POST['project_stmt']), ENT_QUOTES);
 	$project_eta = $_POST['project_eta'] ;
 	$type = $_POST['type'] ;
+	$image = $_POST['img'] ;
+	$project_st = $image." ".$project_sttext ;
 if (strlen($project_st) < 1000) {
         mysqli_query($db_handle, "INSERT INTO projects (user_id, project_title, stmt, project_ETA, project_type) 
                                   VALUES ('$user_id', '$project_title', '$project_st', '$project_eta', '$type');");
+       $idp = mysqli_insert_id($db_handle);                           
+     mysqli_query($db_handle, "INSERT INTO teams (user_id, project_id, team_name, team_owner) VALUES ('$user_id', '$idp', 'defaultteam', '$user_id') ;" ) ;                             
     if(mysqli_error($db_handle)) { echo "Failed to Post Project!"; }
 	else { echo "Project posted succesfully!"; }
 }
@@ -21,6 +25,8 @@ if (strlen($project_st) < 1000) {
         $id = mysqli_insert_id($db_handle);
         mysqli_query($db_handle, "INSERT INTO projects (user_id, blob_id, project_title, stmt, project_ETA, project_type) 
                                 VALUES ('$user_id', '$id', '$project_title', ' ', '$project_eta', '$type');");
+	$idp = mysqli_insert_id($db_handle);
+	mysqli_query($db_handle, "INSERT INTO teams (user_id, project_id, team_name, team_owner) VALUES ('$user_id', '$idp', 'defaultteam', '$user_id') ;" ) ;
 	if(mysqli_error($db_handle)) { echo "Failed to Post Project!"; }
 	else { echo "Project posted succesfully!"; }
 }
