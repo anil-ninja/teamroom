7e7cc2a3bf519d3abb9cdf97bc94ec2ddc42a34a
neerajdexter <neerajdexter@gmail.com>
2014-11-01 21:03:41 +0530

    Navbar and left pannel

--- html_comp/navbar_homepage.php
+++ html_comp/navbar_homepage.php
@@ -2,7 +2,7 @@
 $requestedPage = basename($_SERVER['REQUEST_URI'], '?' . $_SERVER['QUERY_STRING']);
 ?>
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-    <div class="container-fluid">
+    <div class="row">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
