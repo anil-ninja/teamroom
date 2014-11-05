1ac7747dbcf0c1f0197ab87ae338ad2e5df5c9f1
anil-ninja <kumar.anil8892@gmail.com>
2014-11-05 18:09:28 +0530

    kanban

--- html_comp/kanban.php
+++ html_comp/kanban.php
@@ -1,28 +1,32 @@
 <?php
-include_once '../lib/db_connect.php';
-   $pro_id = 1 ;//$_SESSION['project_id'] ;
-   //$user_id = $_SESSION['user_id'] ;
-   $team_name = "ninja";//$_SESSION['team_name'] ;
-   //echo $pro_id." ".$user_id ;
+//include_once '../lib/db_connect.php';
+   $pro_id = $_SESSION['project_id'] ;
+   $user_id = $_SESSION['user_id'] ;
+   $team_name = $_SESSION['team_name'] ;
+
   include_once 'functions/delete_comment.php';
 	$td1 = "" ;
 	
 			
-   $kanban1 = mysqli_query($db_handle, "select DISTINCT a.challenge_id, a.challenge_title, b.first_name from challenges as a join user_info as b 
+   $open_chalange_of_project = mysqli_query($db_handle, "select DISTINCT a.challenge_id, a.challenge_title, b.first_name from challenges as a join user_info as b 
 										WHERE a.project_id = '$pro_id' AND (a.challenge_type = '1' or a.challenge_type = '2') and a.challenge_status = '1'
 										and a.user_id = b.user_id ;") ;
-	while($kanban1row = mysqli_fetch_array($kanban1)) {
-		$name1 = $kanban1row['first_name'] ;
-		$challenge_id11 = $kanban1row['challenge_id'] ;
-		$challenge_title11 = $kanban1row['challenge_title'] ;
+	while($open_chalange_of_projectrow = mysqli_fetch_array($open_chalange_of_project)) {
+		$name1 = $open_chalange_of_projectrow['first_name'] ;
+		$challenge_id11 = $open_chalange_of_projectrow['challenge_id'] ;
+		$challenge_title11 = $open_chalange_of_projectrow['challenge_title'] ;
 		 $td1 .= "<td><p style='font-size: 10px;'>Created By ".$name1."</p><br/>".$challenge_title11."<br/></td>";		
 		}
+		
 	$td2 = "" ;
 	$td5 = "" ;
 	$td3 = "" ;
 	$td4 = "" ;
-	$kanban2 = mysqli_query($db_handle, "select DISTINCT a.user_id, b.username, b.first_name, b.rank from teams as a join user_info as b where a.project_id = '$pro_id'
-										 and a.team_name = '$team_name' and a.user_id = b.user_id ;") ;
+	$kanban2 = mysqli_query($db_handle, "select a.user_id, b.username, b.first_name, b.rank 
+											from teams as a join user_info as b 
+											where a.project_id = '$pro_id'
+												and a.team_name = '$team_name' 
+												and a.user_id = b.user_id ;") ;
 	while($kanban2row = mysqli_fetch_array($kanban2)) {
 		$name2 = $kanban2row['first_name'] ;
 		$username2 = $kanban2row['username'] ;
@@ -38,28 +42,46 @@ include_once '../lib/db_connect.php';
 			$challenge_title12 = $kanban3row['challenge_title'] ;
 			$status3 = $kanban3row['challenge_status'] ;
 		if($status3 == 2){
-			$td2 .= "<p style='font-size: 10px;'>Created By ".$name3."</p><br/>".$challenge_title12."<br/>";
+			$td2 .= "<p style='font-size: 10px;'>Created By ".$name3."</p><br/>".$challenge_title12."<hr/>";
 			}	
 		if($status3 == 4){
-			$td3 .= "<p style='font-size: 10px;'>Created By ".$name3."</p><br/>".$challenge_title12."<br/>";
+			$td3 .= "<p style='font-size: 10px;'>Created By ".$name3."</p><br/>".$challenge_title12."<hr/>";
 			}
 		if($status3 == 5){	
-			$td4 .= "<p style='font-size: 10px;'>Created By ".$name3."</p><br/>".$challenge_title12."<br/>";
+			$td4 .= "<p style='font-size: 10px;'>Created By ".$name3."</p><br/>".$challenge_title12."<hr/>";
 			}
 		}
 	$td5 .=	"<tr><td>".$name2." <br/>".$rank2."</td><td>".$td2."</td><td>".$td3."</td><td>".$td4."</td></tr>" ;
+	$td2 = "" ;
+	$td3 = "" ;
+	$td4 = "" ;
 		}
-	echo "<table class='table table-striped'>
-			<thead><tr><th>Open</th></tr></thead>
-			<tbody><tr>".$td1."</tr></tbody>
-			</table>" ;
-	echo "<table class='table table-striped'>
-			<thead><tr>
-				<th>Open</th>
-				<th>Open</th>
-				<th>Open</th>
-				<th>Open</th>
-			</tr></thead>
-			<tbody>".$td5."</tbody>
-			</table>" ;									 	
+	echo "<table class='table table-striped' border='1'>
+			<thead>
+				<tr>
+					<th>Open</th>
+					<th>
+						<table>
+							<thead>
+								<tr>
+									<th>Team Members</th>
+									<th>In - Progress</th>
+									<th>In - Review</th>
+									<th>Completed</th>
+								</tr>
+							</thead>
+						</table>
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>".$td1."
+					<td><table border='1'>
+							<thead><tr><th></th><th></th><th></th><th></th></tr></thead>
+							<tbody>".$td5."</tbody>
+						</table>
+					</td>
+				</tr>
+			</tbody>
+		  </table>" ;
 ?>
