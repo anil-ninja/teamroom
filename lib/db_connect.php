b25454c7693767e88c1db11a97989b02ff05ce91
rahul lahoria <rahullahoria@cap-lap-360.(none)>
2014-11-09 05:53:20 +0530

    mail Function and logout conform

--- lib/db_connect.php
+++ lib/db_connect.php
@@ -1,6 +1,6 @@
 <?php
-
-$db_handle = mysqli_connect("localhost","root","redhat11111p","ninjasTeamRoom");
+ $config = parse_ini_file('config.ini', true);
+$db_handle = mysqli_connect($config['host'], $config['user'], $config['password'], $config['database']);
 if (mysqli_connect_errno()) {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
--- models/profile.php
+++ models/profile.php
@@ -10,8 +10,7 @@ class profile{
         //$this->setValues($id);
     }
     function setValues($username){
-        //include_once '../lib/db_connect.php';
-        $db_handle = mysqli_connect("localhost","root","redhat111111","ninjasTeamRoom");
+       $db_handle = mysqli_connect("localhost","root","redhat111111","ninjasTeamRoom");
         if (mysqli_connect_errno()) {
             echo "Failed to connect to MySQL: " . mysqli_connect_error();
         }
