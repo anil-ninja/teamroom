b3fc9fd133d671376de8e6af9b84921e832e703c
Rajnish <rajnish_pawar90@yahoo.com>
2014-11-11 20:23:41 +0530

    verify email page has been added(being tested)
    verify email page has been added(being tested)

--- verifyEmail.php
+++ verifyEmail.php
@@ -1,9 +1,44 @@
 <?php
+include_once "lib/db_connect.php";
+include_once "models/rank.php";
 
-$url_value = 
-$hash_key= $_GET['hash_key'];
-$user_access_aid = $_GET['id'];
+$url_value = $_GET['hash_key'];
+$hash_key_access_id = explode('.', $url_value);
+$hash_key= $hash_key_access_id[0];
+$user_access_aid = $hash_key_access_id[1];
 
+$verify_check = mysqli_query($db_handle, "SELECT user_id FROM user_access_aid WHERE hash_key ='$hash_key' AND id = $user_access_aid;");
+$accessed_or_not = mysqli_num_rows($verify_check);
+if ($accessed_or_not == 1) {
+    $verify_checkRow = mysqli_fetch_array($verify_check);
+    $verify_check_user_id = $verify_checkRow['user_id'];
+    mysqli_query ($db_handle, "UPDATE user_access_aid SET status='1' WHERE user_id = $verify_check_user_id;");
+    $response = mysqli_query($db_handle,"select * from user_info where user_id = $verify_check_user_id;") ;
+	$num_rows = mysqli_num_rows($response);
+	if ( $num_rows){
+		//echo "hi";
+		//header('Location: ninjas.php');
+		$responseRow = mysqli_fetch_array($response);
+		$id = $responseRow['user_id'];
+		$lastlogintime = $responseRow['last_login'];
+		$_SESSION['last_login'] = $lastlogintime ;
+		$_SESSION['user_id'] = $id ;
+		$_SESSION['first_name'] = $responseRow['first_name'] ;
+		$_SESSION['username'] = $responseRow['username'] ;
+		$_SESSION['email'] = $responseRow['email'];
+		$logintime = date("y-m-d H:i:s") ;
+		$obj = new rank($id);
+		$new_rank = $obj->user_rank ;
+		mysqli_query($db_handle,"UPDATE user_info SET last_login = '$logintime', rank = '$new_rank' where user_id = '$id' ;" ) ;
+		//$obj = new rank($id);
+		$_SESSION['rank'] = $obj->user_rank;
+		exit;
+	}
+	mysqli_close($db_handle);
+}
+else {
+    
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -33,6 +68,16 @@ $user_access_aid = $_GET['id'];
             </div>
         </div>
     </div>
-        
+        <div class="jumbotron">
+            <?php 
+                if ($accessed_or_not == 0) {
+                    echo "<p align='center'>Something going wrong, Please try again<p>";
+                }
+                else {
+                    echo "<p align='center'>Please wait while your email has been verified</p>";
+                }
+            ?>
+            
+        </div>
     </body>
 </html>
