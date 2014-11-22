6f14fb6fea168768918739497d316e418f109904
rahul lahoria <rahullahoria@cap-lap-360.(none)>
2014-11-23 00:03:59 +0530

    code

--- models/rank.php
+++ models/rank.php
@@ -12,7 +12,7 @@ class rank{
     }
     function setRank(){
         
-        $db_handle = mysqli_connect("localhost","root","redhat111111","ninjasTeamRoom");
+        $db_handle = mysqli_connect("localhost","root","redhat11111p","ninjasTeamRoom");
         if (mysqli_connect_errno()) {
             echo "Failed to connect to MySQL: " . mysqli_connect_error();
         }
