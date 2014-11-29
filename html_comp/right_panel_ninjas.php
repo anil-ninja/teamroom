b86f20c53b1baf9286acc11acc30dbe948424202
neerajdexter <neerajdexter@gmail.com>
2014-11-29 13:52:43 +0530

    right pannel ninjas :)

--- html_comp/right_panel_ninjas.php
+++ html_comp/right_panel_ninjas.php
@@ -111,7 +111,6 @@
         </div>
     </div>
 </div>
-</div>
  <!-- Modal -->
             <div class="modal fade" id="myreminder" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
