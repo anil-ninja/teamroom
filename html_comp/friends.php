4dc3e492ff105e8d00b96ea08976cc5205f0177c
neerajdexter <neerajdexter@gmail.com>
2014-11-29 22:36:24 +0530

    some BIG

--- html_comp/friends.php
+++ html_comp/friends.php
@@ -1,5 +1,8 @@
 <div class="bs-component">
-<div style="padding-top:20px;">
+<div class="list-group">
+<p class="glyphicon glyphicon-comment">
+collap chat<br/><br/>
+</p>
 <?php
 $idb = 0 ;
 $userProjects = mysqli_query($db_handle, "SELECT * FROM user_info as a join (SELECT DISTINCT b.user_id FROM teams as a join teams as b 
@@ -12,12 +15,19 @@ while ($userProjectsRow = mysqli_fetch_array($userProjects)) {
 	$useridFriends = $userProjectsRow['user_id'];
 	$tooltip = ucfirst($friendFirstName)." ".ucfirst($friendLastName);
 		   
-	echo "<a href=\"javascript:void(0)\" onclick=\"javascript:chatWith('".$usernameFriends."')\">
-			<img src='uploads/profilePictures/$usernameFriends.jpg'  style='width:30px; height:30px; margin-bottom:5px;' onError=this.src='img/default.gif' class='img-circle img-responsive'>
-		  </a>";
+	echo "<div class ='row' style='border-width: 1px; border-style: solid;margin:4px;background : #E1F9E4;'>
+			<a href=\"javascript:void(0)\" onclick=\"javascript:chatWith('".$usernameFriends."')\">
+			<div class ='col-md-3 ' style='padding:1px;'>
+				<img src='uploads/profilePictures/$usernameFriends.jpg'  style='width:30px; height:30px;' onError=this.src='img/default.gif' class='img-circle img-responsive'>
+			</div>
+			 <div class = 'col-md-8' style='font-size:10px;padding-top: 5px;'>"
+			.ucfirst($friendFirstName)." ".ucfirst($friendLastName)."
+			</div>
+		  </a>
+		  </div>";
 }
 ?>
-
+</div>
 </div>
 <div class='footer' id='chatform' style='margin-left: 1000px; margin-right: 50px; margin-bottom: 50px; height: 300px; display: none ;'><span class='badge' style='margin-right: 0; cursor: pointer; margin-bottom: 345px; margin-left: 280px;' onclick='closechat()'>x</span></div>  
 <div class='footer' id='chatformdata' style='margin-left: 1005px; margin-right: 65px; margin-bottom: 65px; height: 270px; padding-top : 0; overflow-y: auto; overflow-x: hidden; display: none ;'>  
