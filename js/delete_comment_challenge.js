2a7c6a614b395dc9d41d317c44de69b1a37171b9
anil-ninja <kumar.anil8892@gmail.com>
2014-12-05 02:49:04 +0530

    send mail

--- js/delete_comment_challenge.js
+++ js/delete_comment_challenge.js
@@ -1,5 +1,6 @@
 function del_project_comment(href){
-    if(confirm("Do u really want to delete this comment?")){
+    bootbox.confirm("Do u really want to delete this comment?", function(result) {
+	if(result){
         var dataString = 'pID='+ href;
         $.ajax({
             type: "POST",
@@ -9,101 +10,59 @@ function del_project_comment(href){
             success: function(result){
                 alert(result);
                 location.reload();
-            }
-        });
-    }
+				}
+			});
+		}
+	});
 }
 function delcomment(href) {
-              if(confirm("Do u really want to delete this comment?")){
-                var dataString = 'cID='+ href;
-                $.ajax({
-                    type: "POST",
-                    url: "ajax/ajax.php",
-                    data: dataString,
-                    cache: false,
-                    success: function(result){
-                        alert(result);
-                        location.reload();
-                    }
-                });
-              }
-            }
+	bootbox.confirm("Do u really want to delete this comment?", function(result) {
+	if(result){
+		var dataString = 'cID='+ href;
+		$.ajax({
+			type: "POST",
+			url: "ajax/ajax.php",
+			data: dataString,
+			cache: false,
+			success: function(result){
+				alert(result);
+				location.reload();
+				}
+			});
+       }
+   });
+ }
 function delChallenge(href) {
-    if(confirm("Do u really want to delete this challenge?")){ 
-    var dataString = 'cID='+ href;
-    $.ajax({
-        type: "POST",
-        url: "ajax/delete_chalange.php",
-        data: dataString,
-        cache: false,
-        success: function(result){
-            alert(result);
-            location.reload();
-        }
+    bootbox.confirm("Do u really want to delete?", function(result) {
+	if(result){ 
+		var dataString = 'cID='+ href;
+		$.ajax({
+			type: "POST",
+			url: "ajax/delete_chalange.php",
+			data: dataString,
+			cache: false,
+			success: function(result){
+				alert(result);
+				location.reload();
+				}
+			});
+		}
     });
-    }
-}
-function delArticle(href) {
-    if(confirm("Do u really want to delete this Article?")){
-        
-    var dataString = 'cID='+ href;
-    $.ajax({
-        type: "POST",
-        url: "ajax/delete_chalange.php",
-        data: dataString,
-        cache: false,
-        success: function(result){
-            alert(result);
-            location.reload();
-        }
-    });
-    }
-}
-function delIdea(href) {
-    if(confirm("Do u really want to delete this Idea?")){
-        
-    var dataString = 'cID='+ href;
-    $.ajax({
-        type: "POST",
-        url: "ajax/delete_chalange.php",
-        data: dataString,
-        cache: false,
-        success: function(result){
-            alert(result);
-            location.reload();
-        }
-    });
-    }
 }
 function delProject(href) {
-    if(confirm("Do u really want to delete this Project? Challenges no longer will be there!!!")){
-        
-    var dataString = 'pID='+ href;
-    $.ajax({
-        type: "POST",
-        url: "ajax/delete_chalange.php",
-        data: dataString,
-        cache: false,
-        success: function(result){
-            alert(result);
-            location.reload();
-        }
+	bootbox.confirm("Do u really want to delete this Project?", function(result) {
+	if(result){
+		var dataString = 'pID='+ href;
+		$.ajax({
+			type: "POST",
+			url: "ajax/delete_chalange.php",
+			data: dataString,
+			cache: false,
+			success: function(result){
+				alert(result);
+				location.reload();
+				}
+			});
+		}
     });
-    }
-}
-function delNote(href) {
-    if(confirm("Do u really want to delete this Note?")){
-
-    var dataString = 'noteID='+ href;
-    $.ajax({
-        type: "POST",
-        url: "ajax/delete_chalange.php",
-        data: dataString,
-        cache: false,
-        success: function(result){
-            alert(result);
-            location.reload();
-        }
-    });
-    }
 }
