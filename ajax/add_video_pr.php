2b8a8e762f921626a19293943182874ac4fbf0b4
anil-ninja <kumar.anil8892@gmail.com>
2014-12-04 18:24:01 +0530

    send mail

--- ajax/add_video_pr.php
+++ ajax/add_video_pr.php
@@ -2,7 +2,7 @@
 session_start();
 include_once "../lib/db_connect.php";
 include_once '../functions/delete_comment.php';
-
+include_once '../functions/collapMail.php';
 //echo "i am not working before post";
 if($_POST['videos']){
 	$user_id = $_SESSION['user_id'];	
@@ -11,6 +11,21 @@ if($_POST['videos']){
 	$challenge_title = $_POST['title'] ;
 	$videodes = $_POST['videodes'] ;
 	$challange = $challangetext."<br/> ".$videodes ;
+	$username = $_SESSION['username'];
+	$info =  mysqli_query($db_handle, "select project_title, project_type from projects where project_id = '$pro_id' ;") ;
+	$inforow = mysqli_fetch_array($info) ;
+	$title = $inforow['project_title'] ;
+	$type = $inforow['project_type'] ;
+	if($type == 2) {
+		$members = mysqli_query($db_handle, "select DISTINCT a.user_id, b.email, b.username from teams as a join user_info as b where a.project_id = '$pro_id' and
+											a.user_id != '$user_id' and a.user_id = b.user_id ;") ;
+		while ($memrow = mysqli_fetch_array($members)){
+			$emails = $memrow['email'] ;
+			$mail = $memrow['username'] ;
+			$body2 = "http://collap.com/profile.php?username=".$mail ;
+			collapMail($emails, $username." Add Video IN Project ".$title, $body2);
+			} 
+		}									
 	if (strlen($challange) < 1000) {
         mysqli_query($db_handle,"INSERT INTO challenges (user_id, project_id, challenge_title, stmt, challenge_open_time, challenge_ETA, challenge_type) 
                                     VALUES ('$user_id', '$pro_id', '$challenge_title', '$challange', '1', '1', '8') ; ") ;
@@ -35,6 +50,5 @@ if($_POST['videos']){
 	}
 mysqli_close($db_handle);
 }
-else echo "Invalid parameters!";
-	
+else echo "Invalid parameters!";	
 ?>
