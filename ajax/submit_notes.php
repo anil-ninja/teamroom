aeeaceba56cd9cb32fc44f7b42a9596339addbdf
anil-ninja <kumar.anil8892@gmail.com>
2014-12-04 18:29:39 +0530

    send mail

--- ajax/submit_notes.php
+++ ajax/submit_notes.php
@@ -2,11 +2,28 @@
 session_start();
 include_once "../lib/db_connect.php";
 include_once '../functions/delete_comment.php';
+include_once '../functions/collapMail.php';
+
 if($_POST['notes']){
 	$user_id = $_SESSION['user_id'] ;
 	$pro_id = $_SESSION['project_id'] ;
 	$notestext = $_POST['notes'] ;
 	$image = $_POST['img'] ;
+	$username = $_SESSION['username'];
+	$info =  mysqli_query($db_handle, "select project_title, project_type from projects where project_id = '$pro_id' ;") ;
+	$inforow = mysqli_fetch_array($info) ;
+	$title = $inforow['project_title'] ;
+	$type = $inforow['project_type'] ;
+	if($type == 2) {
+		$members = mysqli_query($db_handle, "select DISTINCT a.user_id, b.email, b.username from teams as a join user_info as b where a.project_id = '$pro_id' and
+											a.user_id != '$user_id' and a.user_id = b.user_id ;") ;
+		while ($memrow = mysqli_fetch_array($members)){
+			$emails = $memrow['email'] ;
+			$mail = $memrow['username'] ;
+			$body2 = "http://collap.com/profile.php?username=".$mail ;
+			collapMail($emails, $username." Create Challenge IN Project ".$title, $body2);
+			} 
+		}
 	$member_project = mysqli_query($db_handle, "select user_id from teams where project_id = '$pro_id' and user_id = '$user_id';");
     if(mysqli_num_rows($member_project) != 0) {
 	if (strlen($image) < 30 ) {
