714430091d16a578aaac8444beed7b268b00814f
Rajnish <rajnish_pawar90@yahoo.com>
2014-12-10 15:31:28 +0530

    new

--- challenges.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once 'ninjas.inc.php';
-if (!isset($_SESSION['first_name'])) {
-    header('Location: index.php');
-}
-
-?>
--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	$requestedPage = $_GET['url'] ;
-    unset($_SESSION['user_id']);
-    unset($_SESSION['first_name']);
-    unset($_SESSION['username']);
-    session_destroy();
-    header('Location: '.$requestedPage);
-?>
