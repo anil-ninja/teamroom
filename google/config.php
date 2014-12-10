9c9eeb6f630b6ad5ff7cfaf9fefc53256e7cf0e3
anil-ninja <kumar.anil8892@gmail.com>
2014-12-10 15:53:30 +0530

    send mail

--- google/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	$clientid	=	'144552641015-toiemq4funembdap9uiqmapeace48sfj.apps.googleusercontent.com';
-	$clientsecret	=	'puZDmvb-g7a6HPCz-LfbLiMO';
-	$redirecturi	=	'https://www.collap.com/google/result.php'; //Add your redirect URl	
-	$maxresults	=	 50;
-?>
