3d03ca3e51c26b78229812cc24545bff6ca7580d
anil-ninja <kumar.anil8892@gmail.com>
2014-12-13 23:43:14 +0530

    challengeOpen page right panel

--- html_comp/kanban.php
+++ html_comp/kanban.php
@@ -1,9 +1,8 @@
 <?php
-include_once 'functions/delete_comment.php';
    $team_name = $_GET['team_name'];
    $team_project_id = $_GET['project_id'];
-$openChallenges = "";
-$open_chalange_of_project = mysqli_query($db_handle, "select challenge_id, challenge_title,creation_time from challenges WHERE project_id = '$team_project_id' 
+	$openChallenges = "";
+$open_chalange_of_project = mysqli_query($db_handle, "select challenge_id, challenge_title, creation_time from challenges WHERE project_id = '$team_project_id' 
 														AND (challenge_type = '1' or challenge_type = '2') and challenge_status = '1' ;");
 while ($open_chalange_of_projectrow = mysqli_fetch_array($open_chalange_of_project)) {
     //$first_name1 = $open_chalange_of_projectrow['first_name'];
@@ -14,11 +13,11 @@ while ($open_chalange_of_projectrow = mysqli_fetch_array($open_chalange_of_proje
     $openChallenges .= "<div class='col-xs-6 col-md-4' style='height:135px;'>
                 <div class='panel panel-default'>
                   <div class = 'panel-heading' style = 'font-size:10px;'>
-                    <span class='glyphicon glyphicon-question-sign'></span>
-                    $challenge_created1
+                    <span class='glyphicon glyphicon-question-sign'></span>".
+                    $challenge_created1."
                   </div>
                   <div class='panel-body' style='padding: 5px;height:90px'>
-                    <a href='challengesOpen.php?challenge_id=$challenge_id11'>" 
+                    <a href='challengesOpen.php?challenge_id=".$challenge_id11."'>" 
                     .ucfirst($challenge_title11)."</a>
                   </div>
                 </div>
@@ -33,20 +32,59 @@ $td4 = "";
     $kanban3 = mysqli_query($db_handle, "select DISTINCT a.challenge_id, a.challenge_title, a.challenge_status, a.challenge_ETA, b.first_name, b.last_name,
 										 b.username from challenges as a join user_info as b join challenge_ownership as c WHERE a.project_id = '$team_project_id' 
 										 AND a.challenge_id = c.challenge_id and c.user_id = b.user_id and a.challenge_status != '3' and a.challenge_status != '7'
-										 and c.user_id IN (select user_id from teams where project_id = '$team_project_id' and team_name = '$team_name' AND member_status = '1' ;)
+										 and c.user_id IN (select user_id from teams where project_id = '$team_project_id' and team_name = '$team_name' AND member_status = '1')
 										 and a.challenge_id NOT IN (select challenge_id from team_tasks WHERE project_id = '$team_project_id' and team_name = '$team_name') ;");
     while ($kanban3row = mysqli_fetch_array($kanban3)) {
         $name3 = $kanban3row['first_name'];
         $lname3 = $kanban3row['last_name'];
         $username3 = $kanban3row['username'];
         $challenge_id12 = $kanban3row['challenge_id'];
-        echo $challenge_id12;
         $challenge_title12 = $kanban3row['challenge_title'];
         $status3 = $kanban3row['challenge_status'];
         $challenge_ETA12 = $kanban3row['challenge_ETA'];
-        if ($status3 == 2) {
-            $td2 .= $name3.", ".$lname3.", ".$username3."<p style='font-size: 10px;'><a href='challengesOpen.php?challenge_id=$challenge_id12'>
-                        ".$challenge_title12."</a></p><hr/>";
+        switch($status3) {
+			case 2:
+				$td2 .= "<div class='col-xs-6 col-md-4' style='height:135px;'>
+                <div class='panel panel-default'>
+                  <div class = 'panel-heading' style = 'font-size:10px;'>
+                    <span class='glyphicon glyphicon-question-sign'></span>
+                  </div>
+                  <div class='panel-body' style='padding: 5px;height:90px'>
+                    <a href='challengesOpen.php?challenge_id=$challenge_id12'>" 
+                    .ucfirst($challenge_title12)."</a>
+                  </div>
+                </div>
+            </div>";
+			break;
+			
+			case 4:
+				$td3 .= "<div class='col-xs-6 col-md-4' style='height:135px;'>
+                <div class='panel panel-default'>
+                  <div class = 'panel-heading' style = 'font-size:10px;'>
+                    <span class='glyphicon glyphicon-question-sign'></span>
+                  </div>
+                  <div class='panel-body' style='padding: 5px;height:90px'>
+                    <a href='challengesOpen.php?challenge_id=$challenge_id12'>" 
+                    .ucfirst($challenge_title12)."</a>
+                  </div>
+                </div>
+            </div>";
+            break;
+            
+            case 5:
+            $td4 .= "<div class='col-xs-6 col-md-4' style='height:135px;'>
+                <div class='panel panel-default'>
+                  <div class = 'panel-heading' style = 'font-size:10px;'>
+                    <span class='glyphicon glyphicon-question-sign'></span>
+                  </div>
+                  <div class='panel-body' style='padding: 5px;height:90px'>
+                    <a href='challengesOpen.php?challenge_id=$challenge_id12'>" 
+                    .ucfirst($challenge_title12)."</a>
+                  </div>
+                </div>
+            </div>";
+            break;
+			}
 
            //  This is commented because ETA is not used in this release     
            //  $delayed_challenges = mysqli_query($db_handle, "SELECT DISTINCT a.challenge_id, b.comp_ch_ETA, b.ownership_creation from challenges as a 
@@ -62,14 +100,6 @@ $td4 = "";
            // }
             
         }
-        if ($status3 == 4) {
-            $td3 .= $name3.", ".$lname3.", ".$username3."<p style='font-size: 10px;'>Created By " . $challenge_id12 . "</p><br/>" . $challenge_title12 . "<hr/>";
-        }
-        if ($status3 == 5) {
-            $td4 .= $name3.", ".$lname3.", ".$username3."<p style='font-size: 10px;'>Created By " . $challenge_id12 . "</p><br/>" . $challenge_title12 . "<hr/>";
-        }
-    }
-    echo $td2."<br/>".$td3."<br/>".$td4 ;
 //<th style='width:150px;'>Delay</th>
 echo "
         <div class='panel panel-default'>
@@ -125,6 +155,16 @@ echo "
         $challenge_id15 = $kanban5row['challenge_id'];
         $challenge_title15 = $kanban5row['challenge_title'];
         $status5 = $kanban5row['challenge_status'];
-        echo $challenge_id15."<br/>".$challenge_title15."<br/>".$status5."<hr/>" ;
+        echo "<div class='col-xs-6 col-md-4' style='height:135px;'>
+                <div class='panel panel-default'>
+                  <div class = 'panel-heading' style = 'font-size:10px;'>
+                    <span class='glyphicon glyphicon-question-sign'></span>
+                  </div>
+                  <div class='panel-body' style='padding: 5px;height:90px'>
+                    <a href='challengesOpen.php?challenge_id=".$challenge_id15."'>" 
+                    .ucfirst($challenge_title15)."</a>
+                  </div>
+                </div>
+            </div>" ;
 		}
 ?>
