b28fbb73339371bedbfc06a58b40625876880154
anil-ninja <kumar.anil8892@gmail.com>
2014-12-13 23:56:58 +0530

    challengeOpen page right panel

--- ajax/add_video.php
+++ ajax/add_video.php
@@ -12,7 +12,7 @@ if($_POST['video']){
 	$challange = $challangetext."<br/> ".$videodes ;
 	if (strlen($challange) < 1000) {
         mysqli_query($db_handle,"INSERT INTO challenges (user_id, challenge_title, stmt, challenge_open_time, challenge_ETA, challenge_type) 
-                                    VALUES ('$user_id', '$challenge_title', '$challange', '1', '1', '8') ; ") ;
+                                    VALUES ('$user_id', '$challenge_title', '$challange', '1', '999999', '8') ; ") ;
 			$idp = mysqli_insert_id($db_handle);
 		involve_in($db_handle,$user_id,"1",$idp); 
 		if(mysqli_error($db_handle)) { echo "Failed to Post Video!"; }
@@ -24,7 +24,7 @@ if($_POST['video']){
         
         $id = mysqli_insert_id($db_handle);
         mysqli_query($db_handle, "INSERT INTO challenges (user_id, challenge_title, blob_id, challenge_open_time, challenge_ETA, stmt, challenge_type) 
-                                VALUES ('$user_id', '$challenge_title', '$id', '1', '1', ' ', '8');");
+                                VALUES ('$user_id', '$challenge_title', '$id', '1', '999999', ' ', '8');");
        $idp = mysqli_insert_id($db_handle);
       involve_in($db_handle,$user_id,"1",$idp); 
 	 if(mysqli_error($db_handle)) { echo "Failed to Post Video!"; }
