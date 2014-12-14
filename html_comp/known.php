4c2432631d193995f13df1f222c62dd0c52de57d
anil-ninja <kumar.anil8892@gmail.com>
2014-12-14 15:06:57 +0530

    recommended friends

--- html_comp/known.php
+++ html_comp/known.php
@@ -48,7 +48,7 @@
                         </div>";
                 }		   
                 else {
-                    echo "<div class ='row' style='border-width: 1px; border-style: solid;margin:4px;background : #E1F9E4;'>
+                    echo "<div class ='row' style='border-width: 1px; border-style: solid;margin:4px;background : rgb(240, 241, 242);'>
                             <div class ='col-md-2' style='padding:1px;'>
                                 <img src='uploads/profilePictures/$usernameFriends.jpg'  onError=this.src='img/default.gif' style='height:40px' class='img-responsive'>
                             </div>
@@ -60,7 +60,7 @@
                             </div>";
                     if (isset($_SESSION['user_id'])) {
                         echo "<div class = 'col-md-2' style='font-size:12px;padding-left: 1px; padding-right: 0px;'>
-                                    <input type = 'submit' class = 'btn-success' onclick='knownperson(".$useridFriends.")' value = 'link'/>
+                                    <input type = 'submit' class = 'btn btn-success' onclick='knownperson(".$useridFriends.")' value = 'link'/>
                             </div>";
                     }
                 echo "</div>";
@@ -70,22 +70,25 @@
 	?>
 </div>
 </div>
-<!---
-<?php 
-	if($profileViewUserID == $user_id) {
-?>
 <div class="panel">
-<p> <b><u> Recommended </u></b></p>
+<p> <b> Recommended </b></p>
  <div>
 	<?php
-		$Recommended = mysqli_query($db_handle, "SELECT * FROM user_info where user_id NOT IN () ORDER by rand() limit 0, 5 ;");
+		$Recommended = mysqli_query($db_handle, "SELECT * FROM user_info where user_id NOT IN (SELECT a.user_id FROM user_info as a join 
+												(SELECT DISTINCT b.user_id FROM teams as a join teams as b where a.user_id = '$user_id' and 
+												a.team_name = b.team_name ) as b where a.user_id = b.user_id) and user_id NOT IN (select a.user_id 
+												FROM user_info as a join known_peoples as b where b.requesting_user_id = '$user_id' and 
+												a.user_id = b.knowning_id and b.status != '4' and b.status != '3')
+												and user_id NOT IN (select a.user_id FROM user_info as a join known_peoples as b
+												where b.knowning_id = '$user_id' and a.user_id = b.requesting_user_id and b.status = '2')
+												 ORDER by rand() limit 0, 5 ;");
 		while ($RecommendedRow = mysqli_fetch_array($Recommended)) {
 			$friendFirstNamer = $RecommendedRow['first_name'];
 			$friendLastNamer = $RecommendedRow['last_name'];
 			$usernameFriendsr = $RecommendedRow['username'];
 			$useridFriendsr = $RecommendedRow['user_id'];
 			$friendRankr = $RecommendedRow['rank'];	     
-			echo "<div class ='row' style='border-width: 1px; border-style: solid;margin:4px;background : #E1F9E4;'>
+			echo "<div class ='row' style='border-width: 1px; border-style: solid;margin:4px;background : rgb(240, 241, 242);'>
 					<div class ='col-md-2' style='padding:1px;'>
 						<img src='uploads/profilePictures/$usernameFriendsr.jpg'  onError=this.src='img/default.gif' style='height:40px' class='img-responsive'>
 					</div>
@@ -97,15 +100,11 @@
 					</div>";
 			if (isset($_SESSION['user_id'])) {
 				echo "<div class = 'col-md-2' style='font-size:12px;padding-left: 1px; padding-right: 0px;'>
-							<input type = 'submit' class = 'btn-success' onclick='knownperson(".$useridFriendsr.")' value = 'link'/>
+							<input type = 'submit' class = 'btn btn-success' onclick='knownperson(".$useridFriendsr.")' value = 'link'/>
 					</div>";
 				}
 			echo "</div>";
-			}
+			} 
 	?>
 </div>
 </div>
-<?php
-	}
-?>
---->
