cc0806a58f6df6ea14ae2a255a1dca1247a814e5
anil-ninja <kumar.anil8892@gmail.com>
2015-01-04 14:40:49 +0530

    notifications

--- ajax/allnotices.php
+++ ajax/allnotices.php
@@ -25,7 +25,7 @@ if ($_POST['all']) {
 						</div>" ;
 			}
 	$notice1 = mysqli_query($db_handle, "(SELECT * FROM events WHERE p_c_id IN (SELECT Distinct p_c_id  FROM involve_in WHERE user_id = '$user_id' and
-										 p_c_type IN ( 8, 12, 18, 19, 20, 21, 22, 23, 24, 25, 28, 29, 30, 36 ) ) and event_creater != '$user_id' )
+										 p_c_type IN ( 3, 4, 5, 6, 7, 10, 11, 13, 14, 15, 16, 17, 31, 33, 38 ) ) and event_creater != '$user_id' )
 										 UNION
 										 (SELECT Distinct * FROM events WHERE event_type IN ( 8, 12, 18, 19, 20, 21, 22, 23, 24, 25, 28, 29, 30, 36 ) 
 										 and p_c_id = '$user_id' and event_creater != '$user_id')
--- ajax/notifications.php
+++ ajax/notifications.php
@@ -32,7 +32,7 @@ if ($_POST['notice']) {
 			$y++ ;
 			}
 	$notice1 = mysqli_query($db_handle, "(SELECT * FROM events WHERE p_c_id IN (SELECT p_c_id  FROM involve_in WHERE user_id = '$user_id' and
-										 p_c_type IN ( 8, 12, 18, 19, 20, 21, 22, 23, 24, 25, 28, 29, 30, 36 )) 
+										 p_c_type IN ( 3, 4, 5, 6, 7, 10, 11, 13, 14, 15, 16, 17, 31, 33, 38 )) 
 										 and timestamp > '$a' and event_creater != '$user_id' )
 										 UNION
 										 (SELECT * FROM events WHERE event_type IN ( 8, 12, 18, 19, 20, 21, 22, 23, 24, 25, 28, 29, 30, 36 ) 
