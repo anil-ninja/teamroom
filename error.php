8f4c9835c5c5d672d21da2f8c162564887aefa55
rajnishp <rajnish_pawar90@yahoo.com>
2015-01-06 01:17:55 +0530

    special char bug resolved

--- error.php
+++ error.php
@@ -4,6 +4,7 @@ include_once 'lib/db_connect.php';
 ?>
 <html lang="en">
     <head>
+        <meta charset="utf-8">
         <title>not exists</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Challenges, Projects, Problem solving, problems">
