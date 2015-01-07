6fdcdce5bc2501c821eb54cac94193fcc03937c6
anil-ninja <kumar.anil8892@gmail.com>
2015-01-07 09:20:15 +0530

    likes

--- functions/delete_comment.php
+++ functions/delete_comment.php
@@ -350,11 +350,10 @@ function recommended_project ($db_handle) {
                                                             ORDER BY rand() LIMIT 5;");
     if (mysqli_num_rows($project_public_title_display2) != 0) { 
         echo "
-                <div class='panel panel-default'>
+                <div class='panel panel-default bs-component'>
                     <div class='panel-heading' style ='padding-top: 0px; padding-bottom: 0px;'>
                         <font size='2'> Recommended</font>
                     </div>
-                    <div class='bs-component'>
                         <table>";
     while ($project_public_title_displayRow2 = mysqli_fetch_array($project_public_title_display2)) {
             $public_pr_titlep2 = str_replace("<s>", "&nbsp;",str_replace("<r>", "'",str_replace("<a>", "&", $project_public_title_displayRow2['project_title']))) ;
@@ -389,8 +388,7 @@ function recommended_project ($db_handle) {
         }
         echo "</td></tr>" ;
         }
-        echo "</table></div>
-        </div>
+        echo "</table>
         </div>";
     }
 }
--- html_comp/left_panel_ninjas.php
+++ html_comp/left_panel_ninjas.php
@@ -30,7 +30,6 @@
 <?php 
     if (isset($_SESSION['user_id'])) {              
  		echo "
-        <div class='nav nav-tabs'>
 			<div class='panel panel-default'>
 				<div class='panel-heading' style ='padding-top: 0px; padding-bottom: 0px;'>
 					<font size='2'>Classified</font>
