c0f1699e61f9cb371c16ae235eba8da08faebc85
anil-ninja <kumar.anil8892@gmail.com>
2015-01-13 18:36:22 +0530

    notifications

--- functions/delete_comment.php
+++ functions/delete_comment.php
@@ -354,7 +354,7 @@ function recommended_project ($db_handle) {
                     <div class='panel-heading' style ='padding-top: 0px; padding-bottom: 0px;'>
                         <font size='2'><b> Recommended</b></font>
                     </div>
-                    <div class='bs-component' style='max-height:130px;overflow-y:scroll;'>
+                    <div class='bs-component' style='max-height:200px;overflow-y:scroll;'>
                         <table>";
     while ($project_public_title_displayRow2 = mysqli_fetch_array($project_public_title_display2)) {
             $public_pr_titlep2 = str_replace("<s>", "&nbsp;",str_replace("<r>", "'",str_replace("<a>", "&", $project_public_title_displayRow2['project_title']))) ;
