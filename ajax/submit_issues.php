0d47b74b80407183d21de2ea623d30a2aeac9f6d
anil-ninja <kumar.anil8892@gmail.com>
2015-01-20 14:09:15 +0530

    project title

---
+++ ajax/submit_issues.php
@@ -0,0 +1,100 @@
+<?php
+session_start();
+include_once "../lib/db_connect.php";
+include_once '../functions/delete_comment.php';
+include_once '../functions/collapMail.php';
+include_once '../functions/image_resize.php';
+if($_POST['notes']){
+	$user_id = $_SESSION['user_id'] ;
+	$pro_id = $_POST['project_id'] ;
+	$notestext = $_POST['notes'] ;
+	$image = $_POST['img'] ;
+	$remaintime = 99 ;
+	$firstname = $_SESSION['first_name'] ;
+	$username = $_SESSION['username'] ;
+	$time = date("Y-m-d H:i:s") ;
+	$info =  mysqli_query($db_handle, "select project_title, project_type from projects where project_id = '$pro_id' ;") ;
+	$inforow = mysqli_fetch_array($info) ;
+	$title = $inforow['project_title'] ;
+	$type = $inforow['project_type'] ;
+	$members = mysqli_query($db_handle, "select DISTINCT a.user_id, b.email, b.username from teams as a join user_info as b where a.project_id = '$pro_id' and
+										a.user_id != '$user_id' and a.user_id = b.user_id ;") ;
+	while ($memrow = mysqli_fetch_array($members)){
+		$emails = $memrow['email'] ;
+		$mail = $memrow['username'] ;
+		$body2 = "Hi, ".$mail." \n \n ".$username." Create Issue IN Project (".$title."). View at \n
+ http://collap.com/project.php?project_id=".$pro_id ;
+		collapMail($emails, " Issue Created IN Project ", $body2);
+		} 
+	if (strlen($image) < 30 ) {
+		$notes = $notestext ;
+		}
+		else {
+			$notes = $image."<br/> ".$notestext ;
+			}
+	$notes_title = $_POST['notes_title'] ;
+	if (strlen($notes) < 1000) {
+		mysqli_query($db_handle,"INSERT INTO challenges (user_id, challenge_title, project_id, stmt, challenge_open_time, challenge_ETA, challenge_type, last_update) 
+									VALUES ('$user_id', '$notes_title', '$pro_id', '$notes', '1', '1', '9', '$time') ; ") ;
+		$idp = mysqli_insert_id($db_handle);
+		involve_in($db_handle,$user_id,"10",$idp); 
+		events($db_handle,$user_id,"10",$idp);
+	} 
+	else {
+		mysqli_query($db_handle, "INSERT INTO blobs (blob_id, stmt)	VALUES (default, '$notes');");
+		$id = mysqli_insert_id($db_handle);
+		mysqli_query($db_handle, "INSERT INTO challenges (user_id, challenge_title, project_id, blob_id, challenge_open_time, challenge_ETA, challenge_type, last_update) 
+								VALUES ('$user_id', '$notes_title', '$pro_id', '$id', '1', '1', '9', '$time');");
+		$idp = mysqli_insert_id($db_handle);
+		involve_in($db_handle,$user_id,"10",$idp); 
+		events($db_handle,$user_id,"10",$idp);
+	}
+	$totallikes = mysqli_query($db_handle, "SELECT * from likes where challenge_id = '$idp' and like_status = '1' ;");
+	if (mysqli_num_rows($totallikes) > 0) { $likes = mysqli_num_rows($totallikes) ;}
+	else { $likes = '' ; }
+	$totaldislikes = mysqli_query($db_handle, "SELECT * from likes where challenge_id = '$idp' and like_status = '2' ;");
+	if (mysqli_num_rows($totaldislikes) > 0) { $dislikes = mysqli_num_rows($totaldislikes) ;}
+	else { $dislikes = '' ; }
+	$data = "" ;
+	$timefunct = date("j F, g:i a") ;
+	$chelange = showLinks(str_replace("<s>", "&nbsp;", str_replace("<r>", "'", str_replace("<a>", "&", $notes)))) ;
+	$title = showLinks(str_replace("<s>", "&nbsp;", str_replace("<r>", "'", str_replace("<a>", "&", $notes_title)))) ;
+	$ntitle = str_replace("<s>", "&nbsp;", str_replace("<r>", "'", str_replace("<a>", "&", $notes_title))) ;
+	$nchallange = str_replace("<s>", "&nbsp;", str_replace("<r>", "'", str_replace("<a>", "&", $notes))) ;
+	$data .= "<div class='list-group asterisk'>
+                <div class='list-group-item'>
+					<div class='dropdown pull-right'>
+						<a href='#'' id='themes' class='dropdown-toggle' data-toggle='dropdown' style='color: #fff'><span class='caret'></span></a>
+						<ul class='dropdown-menu'>
+							<li><a class='btn-link' href='#' onclick='edit_content(\"".$idp."\", 1)'>Edit</a></li>
+							<li><a class='btn-link' href='#' onclick='delChallenge(\"".$idp."\", 3);'>Delete</a></li>
+						</ul>
+					</div>" ;
+    $data = $data ."<span style='font-family: Tenali Ramakrishna, sans-serif; ' id='challenge_ti_".$idp."' class='text'><b>
+					<a style='color:#3B5998;font-size: 26px;' href='challengesOpen.php?challenge_id=".$idp."' target='_blank'>".ucfirst($title)."</a>
+					</b></span><br/><input type='text' class='editbox' style='width : 90%;' id='challenge_title_".$idp."' value='".$ntitle."'/>
+					<span class='icon-asterisk'></span>
+					<span style= 'color: #808080;'>
+					&nbspBy: <a href ='profile.php?username=" . $username . "' style= 'color: #808080;'>".ucfirst($firstname)."</a> | ".$timefunct." | </span>
+                    <span class='icon-hand-up' style='cursor: pointer;color: #808080;' onclick='like(\"".$idp ."\", 1)'>
+                        <input type='submit' class='btn-link' id='likes_".$idp ."' value='".$likes."'/></span> &nbsp
+                    <span class='icon-hand-down' style='cursor: pointer;color: #808080;' onclick='dislike(\"".$idp ."\", 2)'>
+                        <input type='submit' class='btn-link' id='dislikes_".$idp ."' value='".$dislikes."'/>&nbsp;</span>
+                    <hr/><span id='challenge_".$idp."' class='text' style='line-height: 25px; font-size: 14px; color: #444;'>".$chelange."</span><br/>" ;
+       $data = $data .editchallenge($nchallange, $idp) ;
+      $data = $data ."<div class='comments_".$idp."'></div>
+					<div id='step15' class='comments clearfix'>
+						<div class='pull-left lh-fix'>
+							<img src='".resize_image("uploads/profilePictures/$username.jpg", 30, 30, 2)."'  onError=this.src='img/default.gif'>&nbsp
+						</div>
+						<input type='text' class='input-block-level' STYLE='width: 83.0%;' id='own_ch_response_".$idp."'
+						 placeholder='Want to know your comment....'/>
+						<button type='submit' class='btn btn-primary' onclick='comment(\"".$idp."\", 1)' style='margin-bottom: 10px;'>
+						<span class='icon-chevron-right'></span></button>
+					</div></div> </div> " ;
+	if(mysqli_error($db_handle)) { echo "Failed to Post!"; }
+	else { echo "Posted succesfully!"."+"."9"."+".$data ; }
+	mysqli_close($db_handle);
+}
+	else echo "Invalid parameters!";
+?>
---
+++ to delete/chat_box/create_team.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+include_once "../lib/db_connect.php";
+include_once '../functions/delete_comment.php';
+include_once '../functions/collapMail.php';
+if ($_POST['team']) {
+	$email = $_POST['email'] ;
+	$team = $_POST['team'] ;
+	$user_id = $_SESSION['user_id'] ;
+	$pro_id = $_SESSION['project_id'];
+	$sql = mysqli_query($db_handle,"SELECT * FROM user_info where email='$email' ;") ;
+	$data = mysqli_fetch_array($sql);
+	$id = $data['user_id'] ;
+	$username = $_SESSION['username'];
+	$info =  mysqli_query($db_handle, "select project_title, project_type from projects where project_id = '$pro_id' ;") ;
+	$inforow = mysqli_fetch_array($info) ;
+	$title = $inforow['project_title'] ;
+	$type = $inforow['project_type'] ;
+	mysqli_query($db_handle, "INSERT INTO teams (user_id, team_name, team_owner, project_id) VALUES 
+								('$id', '$team', '0', '$pro_id'),
+								('$user_id','$team', '$user_id', '$pro_id');");
+	events($db_handle,$user_id,"11",$pro_id) ;
+	involve_in($db_handle,$user_id,"11",$pro_id) ;
+	$members = mysqli_query($db_handle, "select DISTINCT a.user_id, b.email, b.username from teams as a join user_info as b where a.project_id = '$pro_id' and
+										a.user_id != '$user_id' and a.user_id = b.user_id and a.member_status = '1' ;") ;
+	while ($memrow = mysqli_fetch_array($members)){
+		$emails = $memrow['email'] ;
+		$mail = $memrow['username'] ;
+		$body2 = "Hi, ".$mail." \n \n ".$username." Create Team IN Project (".$title."). View at \n
+http://collap.com/project.php?project_id=".$pro_id ;
+		collapMail($emails, " Team  Created ", $body2);
+		} 						
+     if(mysqli_error($db_handle)) { echo "Failed to Create Team!"; }
+	else { echo "Team Created Successfully !!!"; }    
+ mysqli_close($db_handle);
+}
+else echo "Invalid parameters!";
+?>
