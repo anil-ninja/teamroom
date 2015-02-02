000f5fa24aa9044c70238666624db0e278db1715
anil-ninja <kumar.anil8892@gmail.com>
2015-02-02 16:14:31 +0530

    fb friends

--- ajax/send_invitation.php
+++ ajax/send_invitation.php
@@ -15,15 +15,24 @@ if($_POST['fname']){
 	$sname = $_POST['sname'] ;
 	$email = $_POST['email'] ;
 	$team = $_POST['team'] ;
-	$password = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 6);;
+	$password = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 6);
     mysqli_query($db_handle,"INSERT INTO user_info (first_name, last_name, email, username, password) 
                                     VALUES ('$fname', '$sname', '$email', '$password', '$password') ; ") ;
     $newuserid = mysqli_insert_id($db_handle);
-	$body2 = "Hi, ".$fname." ".$sname." \n \n ".$senderfname." ".$senderlname." added You in team \n
-To know details login to http://collap.com/. \n \n
-            Username: ".$email." \n \n
-            Password: ".$password ;
-	collapMail($email, " Invitation To Join ", $body2) ;
+    $body2 = file_get_contents('../html_comp/mailheader.php')."<body bgcolor='#f6f6f6'><table class='body-wrap'><tr><td></td><td class='container' bgcolor='#FFFFFF'>
+<div class='content'><table><tr><td><img style='width:108px' src = 'http://collap.com/img/collap.gif'/><i style='font-size:58px;'>collap.com</i></td></tr><tr><td>
+<h2>Invitation To Join</h2><p>Hi ".ucfirst($fname)." ".ucfirst($sname).",</p>
+<p>".ucfirst($senderfname)." ".ucfirst($senderlname)." added You in team.</p>
+<p>To know details login to http://collap.com/ </p>
+<p>Your Username is: ".$email."</p>
+<p>Your Password is: ".$password."</p>
+<table><tr><td class='padding'><p><a href='http://collap.com/profile.php?username=".$senderlname."' class='btn-primary'>Click Here to View</a></p></td></tr><tr><td>
+<p> Lets Collaborate!!! Because Heritage is what we pass on to the Next Generation.</p></td></tr></table>
+<p>Thanks,</p><p>Collap Team</p>
+<p><a href='http://twitter.com/collapcom'>Follow @collapcom on Twitter</a></p></td></tr></table>
+</div>
+</td><td></td></tr></table></body></html>" ;
+	collapMail($friendFirstName, "Invitation To Join ", $body2);
 	if($team != "") { 
 		mysqli_query($db_handle, "INSERT INTO teams (user_id, team_name, project_id) VALUES ('$newuserid', '$team', '$pro_id');");
 	}
