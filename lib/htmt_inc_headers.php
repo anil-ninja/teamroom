73db038061a1b9e5b82fa16aa1316c04724c4f43
rahul lahoria <rahullahoria@CAP-LAP-447.(none)>
2015-02-02 20:27:14 +0530

    emotions

--- lib/htmt_inc_headers.php
+++ lib/htmt_inc_headers.php
@@ -52,8 +52,9 @@
 <!--</script>-->
 <script src="js/jquery.js"> </script>
 <script type="text/javascript" src="js/jquery.autosize.js"></script>
-<script type="text/javascript" src="js/jquery.emotions.js"></script>
+
 <link type="text/css" rel="stylesheet" media="all" href="css/chat.css" />
 <link type="text/css" rel="stylesheet" media="all" href="css/screen.css" />
 <link type="text/css" rel="stylesheet" media="all" href="css/introjs.css" />
 <link type="text/css" rel="stylesheet" media="all" href="css/jquery.jscrollpane.css" />
+<script type="text/javascript" src="js/jquery.emotions.js"></script>
