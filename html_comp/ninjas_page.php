8dc2b1e8d4b7929350fbe3e513abbc9fb6ee43fa
anil-ninja <kumar.anil8892@gmail.com>
2015-02-04 20:55:26 +0530

    fb friends

--- html_comp/ninjas_page.php
+++ html_comp/ninjas_page.php
@@ -12,8 +12,7 @@
         <i class="icon-lightbulb"></i><span onclick='show_form_h(11)' style="cursor: pointer;color: #000;font-family: Tenali Ramakrishna, sans-serif;font-size:20px;"> Ideas</span>
        <!-- | 
         <span class="icon-link" onclick='show_form_h(12)' style="cursor: pointer;color: #000;"> Share Links</span> -->
-    </div>
-    <div class='list-group-item'>
+    <hr/>
 		<div id='textForm' ><p style="color: grey;">Please Select Post Type From Above ......</p></div>
 		<div id='remindervalue'></div>
     </div>
