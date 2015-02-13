3e51727892fc75a8cd2fcc7da7b4f9eca240285f
anil-ninja <kumar.anil8892@gmail.com>
2015-02-13 23:47:25 +0530

    special char (+)

---
+++ ajax/submit_link.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+include_once "../lib/db_connect.php";
+include_once '../functions/delete_comment.php';
+include_once '../functions/image_resize.php';
+if($_POST['title']){
+	$user_id = $_SESSION['user_id'];
+	$text = $_POST['description'] ;
+	$title = $_POST['title'] ;
+	$url = $_POST['url'] ;
+	$author = $_POST['author'] ;
+	$image = $_POST['img'] ;
+	$post_id = $_POST['id'] ;
+	if ($post_id == "NaN") {
+		$pro_id = 0;
+	}
+	else { $pro_id = $post_id ; }
+	$article = $image." <br/>".$text." <br/> <a href=\'http://".$url."\' target=\'_blank\'> .. more </a> <br/> By : ".ucfirst($author) ;
+	$time = date("Y-m-d H:i:s") ;
+	$remaintime = 99 ;
+	$firstname = $_SESSION['first_name'] ;
+	$username = $_SESSION['username'] ;
+	if (strlen($article) < 1000) {
+        mysqli_query($db_handle,"INSERT INTO challenges (user_id, project_id, challenge_title, stmt, challenge_open_time, challenge_ETA, challenge_type, last_update) 
+                                    VALUES ('$user_id', '$pro_id', '$title', '$article', '1', '1', '10', '$time') ; ") ;
+		$idp = mysqli_insert_id($db_handle);
+	} 
+	else {
+		mysqli_query($db_handle, "INSERT INTO blobs (blob_id, stmt) VALUES (default, '$article');");
+		$id = mysqli_insert_id($db_handle);
+		mysqli_query($db_handle, "INSERT INTO challenges (user_id, project_id, challenge_title, blob_id, challenge_open_time, challenge_ETA, stmt, challenge_type, last_update) 
+								VALUES ('$user_id', '$pro_id', '$title', '$id', '1', '1', ' ', '10', '$time');");
+		$idp = mysqli_insert_id($db_handle); 
+	}
+	mysqli_query($db_handle,"insert into involve_in (user_id, p_c_id, p_c_type) VALUES ('$user_id', '$idp', '1'),('$user_id', '$idp', '3'),('$user_id', '$idp', '5'),('$user_id', '$idp', '9') ;") ;
+	$totallikes = mysqli_query($db_handle, "SELECT * from likes where challenge_id = '$idp' and like_status = '1' ;");
+	if (mysqli_num_rows($totallikes) > 0) { $likes = mysqli_num_rows($totallikes) ;}
+	else { $likes = '' ; }
+	$totaldislikes = mysqli_query($db_handle, "SELECT * from likes where challenge_id = '$idp' and like_status = '2' ;");
+	if (mysqli_num_rows($totaldislikes) > 0) { $dislikes = mysqli_num_rows($totaldislikes) ;}
+	else { $dislikes = '' ; }
+	$data = "" ;
+	$timefunct = date("j F, g:i a") ;
+	$chelange = showLinks(str_replace("<s>", "&nbsp;", str_replace("<r>", "'", str_replace("<a>", "&",str_replace("<an>", "+", $article))))) ;
+	$ptitle = showLinks(str_replace("<s>", "&nbsp;", str_replace("<r>", "'", str_replace("<a>", "&",str_replace("<an>", "+", $title))))) ;
+	$ntitle = str_replace("<s>", "&nbsp;", str_replace("<r>", "'", str_replace("<a>", "&",str_replace("<an>", "+", $title)))) ;
+	$nchallange = str_replace("<s>", "&nbsp;", str_replace("<r>", "'", str_replace("<a>", "&",str_replace("<an>", "+", $article)))) ;
+	$data .= "<div class='list-group idea'>
+                <div class='list-group-item'>
+					<div class='dropdown pull-right'>
+						<a href='#'' id='themes' class='dropdown-toggle' data-toggle='dropdown' style='color: #fff'><span class='caret'></span></a>
+						<ul class='dropdown-menu'>
+							<li><a class='btn-link' href='#' onclick='edit_content(\"".$idp."\", 1)'>Edit</a></li>
+							<li><a class='btn-link' href='#' onclick='delChallenge(\"".$idp."\", 3);'>Delete</a></li>
+						</ul>
+					</div>" ;
+    $data = $data ."<span style='font-family: Tenali Ramakrishna, sans-serif;' id='challenge_ti_".$idp."' class='text'><b>
+					<a style='color:#3B5998;font-size: 26px;' href='challengesOpen.php?challenge_id=".$idp."' target='_blank'>".ucfirst($ptitle)."</a>
+					</b></span><input type='text' class='editbox' style='width : 90%;' id='challenge_title_".$idp."' value='".$ntitle."'/><br/>
+					<span class='icon-globe'></span>
+					<span style= 'color: #808080;'>
+					&nbspBy: <a href ='profile.php?username=" . $username . "' style= 'color: #808080;'>".ucfirst($firstname)."</a> | ".$timefunct."</span>
+                    <hr/><span id='challenge_".$idp."' class='text' style='line-height: 25px; font-size: 14px; color: #444;'>".$chelange."</span><br/>" ;
+       $data = $data .editchallenge($nchallange, $idp) ;
+      $data = $data ."<hr/><div class='row-fluid'><div class='col-md-1'>".share_challenge($idp)."</div><div class='col-md-5'>| &nbsp;&nbsp;&nbsp;
+						<span class='icon-hand-up' style='cursor: pointer;' onclick='like(\"".$idp ."\", 1)'> <b>Push</b>
+                        <input type='submit' class='btn-link' id='likes_".$idp ."' value='".$likes."'/> |</span> &nbsp;&nbsp;&nbsp;
+                    <span class='icon-hand-down' style='cursor: pointer;' onclick='dislike(\"".$idp ."\", 2)'> <b>Pull</b>
+                        <input type='submit' class='btn-link' id='dislikes_".$idp ."' value='".$dislikes."'/>&nbsp;</span></div></div><hr/>
+                        <div class='comments_".$idp."'></div>
+					<div id='step15' class='comments clearfix'>
+						<div class='pull-left lh-fix'>
+							<img src='".resize_image("uploads/profilePictures/$username.jpg", 30, 30, 2)."'  onError=this.src='img/default.gif'>&nbsp
+						</div>
+						<input type='text' class='input-block-level' STYLE='width: 83.0%;' id='own_ch_response_".$idp."'
+						 placeholder='Want to know your comment....'/>
+						<button type='submit' class='btn btn-primary' onclick='comment(\"".$idp."\", 1)' style='margin-bottom: 10px;'>
+						<span class='icon-chevron-right'></span></button>
+					</div></div> </div> " ; 
+	if(mysqli_error($db_handle)) { echo "Failed To Post"; }
+	else {  echo "Posted"."|+".$data ; }
+	mysqli_close($db_handle);
+	} 
+	else echo "Invalid parameters!";
+?>
---
+++ js/getMeta.js
@@ -0,0 +1,158 @@
+function url_domain(data) {
+  var    a      = document.createElement('a');
+         a.href = data;
+  return a.hostname;
+}
+    jQuery.ajax=function(e){function o(e){return!r.test(e)&&/:\/\//.test(e)}var t=location.protocol,n=location.hostname,r=RegExp(t+"//"+n),i="http"+(/^https/.test(t)?"s":"")+"://query.yahooapis.com/v1/public/yql?callback=?",s='select * from html where url="{URL}" and xpath="*"';return function(t){var n=t.url;if(/get/i.test(t.type)&&!/json/i.test(t.dataType)&&o(n)){t.url=i;t.dataType="json";t.data={q:s.replace("{URL}",n+(t.data?(/\?/.test(n)?"&":"?")+jQuery.param(t.data):"")),format:"xml"};if(!t.success&&t.complete){t.success=t.complete;delete t.complete}t.success=function(e){return function(t){if(e){e.call(this,{responseText:(t.results[0]||"").replace(/<script[^>]+?\/>|<script(.|\s)*?\/script>/gi,"")},"success")}}}(t.success)}return e.apply(this,arguments)}}(jQuery.ajax);
+
+function getUrlData(url_get){
+	var url_data = {};
+	$.ajax({
+		url: url_get,
+		type: "GET",
+		async: false
+	}).done(function (response) {
+		var div = document.createElement("div"),
+			responseText = response.results[0],
+			title, metas, meta, name, description, metaImg, property, i, author;
+		//console.log(response.results[0]);
+		div.innerHTML = responseText;
+		//console.log(div.getElementsByTagName("meta").length);
+		title = div.getElementsByTagName("title");
+		title = title.length ? title[0].innerHTML : undefined;
+		
+		metas = div.getElementsByTagName("meta");
+		//console.log(metas);
+		description = "";
+		for (i = 0; i < metas.length; i++) {
+		//console.log(metas.length,i,metas[i].outerHTML);
+			name = metas[i].getAttribute("name");
+			if (name === "description") {
+				meta = metas[i];
+				description = meta.getAttribute("content");
+				break;
+			}
+		}
+		if(description == ""){
+
+			for (i = 0; i < metas.length; i++) {
+				name = metas[i].getAttribute("name");
+				//console.log(name);
+				if (name === "twitter:description") {
+					meta = metas[i];
+					//console.log(meta);
+					description = meta.getAttribute("value")?meta.getAttribute("value"):meta.getAttribute("content");
+					//break;
+				}
+			}
+
+		}
+		
+		for (i = 0; i < metas.length; i++) {
+			name = metas[i].getAttribute("property");
+		//console.log(metas.length,i,metas[i].outerHTML);
+			if (name === "author") {
+				meta = metas[i];
+				author = meta.getAttribute("content");
+				break;
+			}
+			name = metas[i].getAttribute("name");
+		   //  console.log(name);
+		if (name === "twitter:creator") {
+				meta = metas[i];
+			//console.log(meta);
+				author = meta.getAttribute("value")?meta.getAttribute("value"):meta.getAttribute("content");
+				break;
+			} else if (name === "twitter:site") {
+				meta = metas[i];
+			//console.log(meta);
+				author = meta.getAttribute("value")?meta.getAttribute("value"):meta.getAttribute("content");
+				break;
+			}
+		}
+		for (i = 0; i < metas.length; i++) {
+			property = metas[i].getAttribute("content");
+			//console.log(property);
+			
+			if (property != null  && property.substr(property.length - 4) === ".jpg") {
+				
+				metaImg = property;
+				break;
+			}
+		}
+
+		if (metaImg == undefined){
+			for (i = 0; i < metas.length; i++) {
+			name = metas[i].getAttribute("name");
+					//console.log(name);
+				if (name === "twitter:image:src") {
+						meta = metas[i];
+					console.log(meta);
+						metaImg = meta.getAttribute("value")?meta.getAttribute("value"):meta.getAttribute("content");
+						break;
+					}
+			}
+
+		}
+		if (metaImg == undefined){
+		
+			imgs = div.getElementsByTagName("img");
+			for (var i=0, n=imgs.length;i<n;i++) 
+			allImg = '<img src="'+imgs[i].src+'" width="60%">';
+			metaImg = imgs[0].src;
+			  
+		}
+    //console.log("Title:", title);
+    //document.getElementById("title").innerHTML = title;
+    //console.log("Description:", description);
+    //document.getElementById("dec").innerHTML = description;
+    
+    //document.getElementById("author").innerHTML = author;
+    
+
+    //console.log("Image:", metaImg);
+    //document.getElementById("allImgD").innerHTML = allImg;
+    /*
+	document.getElementById("imgD").innerHTML = "<img src=\'"+metaImg+"\' height= \'200px\'/>";
+    */
+    
+		url_data.title = title;
+		url_data.description = description;
+		url_data.domain = url_domain(url_get);
+		url_data.author = url_data.author?author:url_data.domain.split(".")[1];
+		url_data.metaImg = metaImg;
+		var Image = "<img src=\""+url_data.metaImg+"\" style=\"max-width: 100%;\" onError=\"this.src=\"img/default.gif\"\" />";
+		var IDPr = parseInt($("#ProjectIDValue").val()) ;
+		var dataString = 'title='+ title + '&description=' + description + '&url=' + url_data.domain 
+						+ '&author=' + url_data.author + '&img=' + Image + '&id=' + IDPr ;
+		$.ajax({
+			type: "POST",
+			url: "ajax/submit_link.php",
+			async: false ,
+			data: dataString,
+			cache: false,
+			success: function(result){
+				var notice = result.split("|+");
+				if(notice['0'] == "Posted"){
+					bootstrap_alert(".alert_placeholder", "Posted Succeccfully !!", 5000,"alert-success");
+					$("#create_link").removeAttr('disabled');
+					$('.loading').remove();
+					$("#sharedlink").val("");
+					$(".newPosts").prepend(notice['1']) ;
+					$(".editbox").hide() ;
+				}
+				else {
+					bootstrap_alert(".alert_placeholder", notice['0'], 5000,"alert-warning");
+					$("#create_link").removeAttr('disabled');
+					$('.loading').remove();
+				}
+			}
+		}); 
+		console.log(url_data);
+		//return  url_data ;
+
+		}).fail(function (jqXHR, textStatus, errorThrown) {
+			console.log("AJAX ERROR:", textStatus, errorThrown);		
+	});
+   
+}
