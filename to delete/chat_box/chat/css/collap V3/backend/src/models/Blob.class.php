0a6de8badcc21beaea4de6d6410d57117bc2d777
RED-FIGHTER(spider-ninja) <spider-ninja-mine@spiderninjamine-Ideapad-S100.(none)>
2015-03-04 02:14:52 +0530

    dao toString toArray

--- to delete/chat_box/chat/css/collap V3/backend/src/models/Blob.class.php
+++ to delete/chat_box/chat/css/collap V3/backend/src/models/Blob.class.php
@@ -7,8 +7,38 @@
 	 */
 	class Blob{
 		
-		var $id;
-		var $stmt;
-		
+		private $id;
+		private $stmt;
+		
+		function __construct($stmt, $id = null){
+			$this->id = $id;
+			$this->stmt = $stmt;
+			
+		}
+		
+		function setId($id){
+			$this -> id = $id;
+		}
+		function getId(){
+				return $this->id;
+		}
+
+		function setStmt($stmt){
+			$this -> stmt = $stmt;
+		}
+		function getStmt(){
+				return $this-> stmt;
+		}
+		
+		function toString (){
+			return $this -> id . ", " . $this -> stmt;
+		}
+		
+		function toArray() {
+			return array (
+						id => $this->id,
+						stmt => $this->stmt
+				);
+		}		
 	}
 ?>
