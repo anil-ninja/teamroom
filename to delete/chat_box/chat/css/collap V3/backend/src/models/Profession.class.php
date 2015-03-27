6401d732e598e8f4d05e3eb7b0bbe0c7da0fc6a2
anil-ninja <kumar.anil8892@gmail.com>
2015-03-27 20:08:44 +0530

    login

---
+++ to delete/chat_box/chat/css/collap V3/backend/src/models/Profession.class.php
@@ -0,0 +1,52 @@
+<?php
+	/**
+	 * Object represents table 'professions'
+	 *
+     	 * @author: http://rahul.com
+     	 * @date: 2015-03-03 14:48	 
+	 */
+	class Profession{
+		
+		private $id;
+		private $name;
+		function __construct($name,$id = null)
+		{
+			$this->id = $id;
+
+		$this ->name = $name;
+
+		
+		}function setId($id){
+			$this -> id = $id;
+		}
+		function getId(){
+				return $this->id;
+		}
+
+		
+			function setName($name){
+			$this -> Name = $name;
+		}
+		function getName(){
+				return $this-> name;
+				
+		}
+		function toString (){
+			return $this -> id . ", " . $this->name;}
+			function toArray() {
+			return array (
+						id => $this->id,
+						
+						
+						name => $this->name
+						
+						);
+
+			
+
+		
+	}
+?>
+
+		
+	
