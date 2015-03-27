2d6c450ba1dfbe5e71c632cb7c1a87000fa098c8
anil-ninja <kumar.anil8892@gmail.com>
2015-03-27 19:50:13 +0530

    login

--- to delete/chat_box/chat/css/collap V3/backend/src/models/Skill.class.php
+++
@@ -1,14 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'skills'
-	 *
-     	 * @author: http://phpdao.com
-     	 * @date: 2015-03-03 14:48	 
-	 */
-	class Skill{
-		
-		var $id;
-		var $name;
-		
-	}
-?>
