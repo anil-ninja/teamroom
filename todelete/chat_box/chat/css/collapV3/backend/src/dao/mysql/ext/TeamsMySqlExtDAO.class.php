12d29f69aab7a0157a25e0e8fadb9643cba31927
rajnishp <rajnish_pawar90@yahoo.com>
2015-04-08 17:52:53 +0530

    get teammembers

--- todelete/chat_box/chat/css/collapV3/backend/src/dao/mysql/ext/TeamsMySqlExtDAO.class.php
+++ todelete/chat_box/chat/css/collapV3/backend/src/dao/mysql/ext/TeamsMySqlExtDAO.class.php
@@ -2,13 +2,13 @@
 /**
  * Class that operate on table 'teams'. Database Mysql.
  *
- * @author: http://phpdao.com
+ * @author: rajnish
  * @date: 2015-03-03 14:48
  */
 class TeamsMySqlExtDAO extends TeamsMySqlDAO{
 
 	/**
-	 * Get all records from table
+	 * Get all team name records from table
 	 */
 	public function queryAllTeamNames($projectId, $teamName){
 		$sql = "SELECT DISTINCT teams.team_name, teams.project_id, user.username 
@@ -21,6 +21,19 @@ class TeamsMySqlExtDAO extends TeamsMySqlDAO{
 	}
 
 	/**
+	 * Get all team member records from table
+	 */
+	public function queryAllTeamMembers($projectId, $teamName){
+		$sql = "SELECT user.first_name, user.last_name, user.username, user.rank 
+				FROM teams as team join user_info as user 
+				WHERE team.project_id= ? AND user.id = team.user_id AND team.member_status = '1' AND team.team_name = ?";
+		$sqlQuery = new SqlQuery($sql);
+		$sqlQuery -> set($projectId);
+		$sqlQuery -> set($teamName);
+		return $this->getListAllTeamMembers($sqlQuery);
+	}
+
+	/**
 	 * Read row
 	 *
 	 * @return TeamsMySql 
@@ -30,12 +43,27 @@ class TeamsMySqlExtDAO extends TeamsMySqlDAO{
 		
 		return $team;
 	}
+
+	protected function readRowAllTeamMembers($row){
+		$team = new TeamMembers($row['first_name'],$row['last_name'],$row['rank'], $row['username']);
+		
+		return $team;
+	}
 	
 	protected function getListAllTeams($sqlQuery){
 		$tab = QueryExecutor::execute($sqlQuery);
 		$ret = array();
 		for($i=0;$i<count($tab);$i++){
 			$ret[$i] = $this->readRowAllTeams($tab[$i]);
+		}
+		return $ret;
+	}
+
+	protected function getListAllTeamMembers($sqlQuery){
+		$tab = QueryExecutor::execute($sqlQuery);
+		$ret = array();
+		for($i=0;$i<count($tab);$i++){
+			$ret[$i] = $this->readRowAllTeamMembers($tab[$i]);
 		}
 		return $ret;
 	}
